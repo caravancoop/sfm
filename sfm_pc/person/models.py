5bad8b9dc9c7d5c24b97916afaf0469b6750f1f0
Guillaume Auger <gauger@caravan.coop>
2015-08-12 13:31:25 -0400

    Add person model

--- sfm_pc/person/models.py
+++ sfm_pc/person/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+import reversion
 
-# Create your models here.
+class Person(models.Model):
+    pass
+
+class PersonName(models.Model):
+    lang = models.CharField(max_length=5, null=False)
+    person = models.ForeignKey(Person)
+    value = models.TextField()
+
+reversion.register(PersonName)
+
+class PersonAlias(models.Model):
+    lang = models.CharField(max_length=5, null=False)
+    person = models.ForeignKey(Person)
+    value = models.TextField()
+
+reversion.register(PersonAlias)
+
+class PersonNotes(models.Model):
+    lang = models.CharField(max_length=5, null=False)
+    person = models.ForeignKey(Person)
+    value = models.TextField()
+
+reversion.register(PersonNotes)
