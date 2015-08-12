c3ba508bdd07eb8b7a93c79da90df64a915f875c
Étienne Latulippe <elatulippe@caravan.coop>
2015-08-12 13:53:05 -0400

    add url person

---
+++ sfm_pc/person/urls.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.conf.urls import patterns, url
+
+from .views import PersonView, PersonCreate, PersonUpdate, PersonDelete, PpPersonCloseView, PpPersonCreate, PpPersonUpdate
+
+urlpatterns = patterns('',
+                       url(r'^$', PersonView.as_view(),
+                           name='person'),
+
+
+
+                       url(r'popup/add/$', PpPersonCreate.as_view(),
+                           name='Pp_add_person'),
+                       url(r'popup/(?P<pk>\d+)/close/$',
+                           PpPersonCloseView.as_view(), name='Pp_close_person'),
+                       url(r'popup/(?P<pk>\d+)/$', PpPersonUpdate.as_view(),
+                           name='Pp_edit_person'),
+                       url(r'add/$', PersonCreate.as_view(),
+                           name='add_person'),
+                       url(r'(?P<pk>\d+)/$', PersonUpdate.as_view(),
+                           name='edit_person'),
+                       url(r'(?P<pk>\d+)/delete/$',
+                           PersonDelete.as_view(), name='delete_person'),
+                       )
