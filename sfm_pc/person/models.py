16cf8b3e6f4d088732cb55ebf8844f79766d173c
Guillaume Auger <gauger@caravan.coop>
2015-08-17 11:33:37 -0400

    Fix the foreign keys and add getters to Person

--- sfm_pc/person/models.py
+++ sfm_pc/person/models.py
@@ -3,25 +3,39 @@ import reversion
 from django.db import models
 
 class Person(models.Model):
-    pass
+    def get_name(self, lang='en'):
+        return self.get_attribute(PersonName, lang)
+
+    def get_alias(self, lang='en'):
+        return self.get_attribute(PersonAlias, lang)
+
+    def get_notes(self, lang='en'):
+        return self.get_attribute(PersonNotes, lang)
+
+    def get_attribute(self, object_type, lang='en'):
+        queryset = object_type.objects.filter(person=self, lang=lang)
+        values = list(queryset[:1])
+        if values:
+            return values[0].value
+        return None
 
 class PersonName(models.Model):
+    person = models.ForeignKey('Person')
     lang = models.CharField(max_length=5, null=False)
-    person = models.ForeignKey(Person)
     value = models.TextField()
 
 reversion.register(PersonName)
 
 class PersonAlias(models.Model):
+    person = models.ForeignKey('Person')
     lang = models.CharField(max_length=5, null=False)
-    person = models.ForeignKey(Person)
     value = models.TextField()
 
 reversion.register(PersonAlias)
 
 class PersonNotes(models.Model):
+    person = models.ForeignKey('Person')
     lang = models.CharField(max_length=5, null=False)
-    person = models.ForeignKey(Person)
     value = models.TextField()
 
 reversion.register(PersonNotes)
