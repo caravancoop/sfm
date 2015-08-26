37dfff814f6a878d38f3392ff324c32e046589e5
Guillaume Auger <gauger@caravan.coop>
2015-08-26 12:13:23 -0400

    Add combomethod decorator

--- sfm_pc/complex_fields/model_decorators.py
+++ sfm_pc/complex_fields/model_decorators.py
@@ -1,3 +1,5 @@
+import functools
+
 from django.db import models
 from django.db.models.signals import class_prepared
 
@@ -20,5 +22,16 @@ def sourced(orig_cls):
     orig_cls.sourced = True
     return orig_cls
 
+class combomethod(object):
+    def __init__(self, method):
+        self.method = method
 
+    def __get__(self, obj=None, objtype=None):
+        @functools.wraps(self.method)
+        def _wrapper(*args, **kwargs):
+            if obj is not None:
+                return self.method(obj, *args, **kwargs)
+            else:
+                return self.method(objtype, *args, **kwargs)
 
+        return _wrapper
