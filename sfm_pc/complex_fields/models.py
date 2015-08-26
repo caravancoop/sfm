4251bcbb80ef2e4e1c15fe962b09bb53305c84f4
Guillaume Auger <gauger@caravan.coop>
2015-08-26 14:50:05 -0400

    Moved some ComplexField methods to ComplexFieldContainer

--- sfm_pc/complex_fields/models.py
+++ sfm_pc/complex_fields/models.py
@@ -3,7 +3,6 @@ import inspect
 from django.db import models
 from django.core.exceptions import ValidationError, FieldDoesNotExist
 from source.models import Source
-from .model_decorators import combomethod
 
 
 class ComplexField(models.Model):
@@ -14,70 +13,7 @@ class ComplexField(models.Model):
         abstract = True
         unique_together = ('object', 'lang')
 
-    @combomethod
-    def translate(self_cls, value, lang, object=None):
-        if inspect.isclass(self_cls):
-            cls = self_cls
-        else:
-            cls = self_cls.__class__
 
-        translations = cls.objects.filter(object=object)
-
-        if not translations.exists():
-            raise FieldDoesNotExist("Can't translate a field that doesn't exist")
-
-        translation = translations.filter(lang=lang)
-        translation = list(translation[:1])
-        if not translation:
-            translation = cls()
-            translation.object = object
-            translation.lang = lang
-        else:
-            translation = translation[0]
-            if translation.value != None:
-                raise ValidationError("Can't translate an already translated field")
-
-        translation.value = value
-        translation.save()
-
-        if hasattr(translation, 'sourced'):
-            with_sources = cls.objects.exclude(sources=None)
-            sources = with_sources[0].sources.all()
-            for src in sources:
-                translation.sources.add(src)
-
-        translation.save()
-
-    @combomethod
-    def update(self_cls, value, lang, sources=[], object=None):
-        if inspect.isclass(self_cls):
-            cls = self_cls
-        else:
-            cls = self_cls.__class__
-
-        translations = cls.objects.filter(object=object)
-
-        for trans in translations:
-            trans.value = None
-            if hasattr(trans, 'sourced'):
-                for source in sources:
-                    trans.sources.clear()
-            trans.save()
-        translation = translations.filter(lang=lang)
-        translation = list(translation[:1])
-        if not translation:
-            translation = cls()
-            translation.object = object
-            translation.lang = lang
-        else:
-            translation = translation[0]
-
-        translation.value = value
-        translation.save()
-
-        if hasattr(translation, 'sourced'):
-            for src in sources:
-                translation.sources.add(src)
 class ComplexFieldContainer(object):
     def __init__(self, table_model, field_model):
         self.table_model = table_model
