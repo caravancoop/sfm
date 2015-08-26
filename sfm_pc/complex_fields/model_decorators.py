15c6adb3b8660d3a03deb9415a25a040b97b7a52
Guillaume Auger <gauger@caravan.coop>
2015-08-26 14:51:54 -0400

    Removed combomethod decorator

--- sfm_pc/complex_fields/model_decorators.py
+++ sfm_pc/complex_fields/model_decorators.py
@@ -21,17 +21,3 @@ def versioned(orig_cls):
 def sourced(orig_cls):
     orig_cls.sourced = True
     return orig_cls
-
-class combomethod(object):
-    def __init__(self, method):
-        self.method = method
-
-    def __get__(self, obj=None, objtype=None):
-        @functools.wraps(self.method)
-        def _wrapper(*args, **kwargs):
-            if obj is not None:
-                return self.method(obj, *args, **kwargs)
-            else:
-                return self.method(objtype, *args, **kwargs)
-
-        return _wrapper
