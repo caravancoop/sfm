222fe72f26dae82821a3ea4e45ca5896884dad1a
Guillaume Auger <gauger@caravan.coop>
2015-08-27 15:43:25 -0400

    Add create_sources Source method

--- sfm_pc/source/models.py
+++ sfm_pc/source/models.py
@@ -10,3 +10,22 @@ CONFIDENCE_LEVELS = (
 class Source(models.Model):
     source = models.TextField()
     confidence = models.CharField(max_length=1, choices=CONFIDENCE_LEVELS)
+
+    @classmethod
+    def create_sources(cls, sources):
+        sources = []
+        for source in sources:
+            possible_source = Source.objects.filter(
+                source=source['source'],
+                confidence=source['confidence']
+            )
+            possible_source = list(possible_source[:1])
+            if possible_source:
+                sources.append(possible_source)
+            else:
+                new_source = cls(source['source'], source['confidence'])
+                new_source.save()
+                sources.append(new_source)
+
+        return sources
+
