41edd4afffbb23e879236ee9c4fb2559cd15cdf7
Guillaume Auger <gauger@caravan.coop>
2015-09-04 17:05:50 -0400

    Remove edit url from person

--- sfm_pc/person/urls.py
+++ sfm_pc/person/urls.py
@@ -8,10 +8,6 @@ urlpatterns = patterns(
         '(?P<person_id>[0-9]+)/',
         FieldUpdate.as_view(),
         name="update_field"),
-    url(r'edit/(?P<field_type>[a-zA-Z]+)/' +
-        '(?P<person_id>[0-9]+)/',
-        FieldUpdate.as_view(),
-        name="update_field"),
     url(r'add/$', PersonCreate.as_view(), name='add_person'),
     url(r'^$', PersonView.as_view(), name='person'),
     url(r'(?P<pk>\d+)/$', PersonUpdate.as_view(), name='edit_person'),
