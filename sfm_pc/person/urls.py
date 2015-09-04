4cc30b57a15b5a73c566a5236cf3a31eb3b5dd47
Guillaume Auger <gauger@caravan.coop>
2015-09-04 16:58:15 -0400

    Remove obsolete from python2

--- sfm_pc/person/urls.py
+++ sfm_pc/person/urls.py
@@ -1,6 +1,3 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.conf.urls import patterns, url
 
 from .views import PersonView, PersonUpdate, PersonCreate, FieldUpdate
--- sfm_pc/person/views.py
+++ sfm_pc/person/views.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.utils.decorators import method_decorator
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
