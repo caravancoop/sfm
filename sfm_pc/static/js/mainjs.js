b1afeb93b7f5329418e17430c2b318e30ca29439
lavanyaks <lavanya.singh24@gmail.com>
2015-09-08 16:39:55 -0400

    JS changes

--- sfm_pc/static/js/mainjs.js
+++ sfm_pc/static/js/mainjs.js
@@ -18,8 +18,6 @@ function grabData() {
 		var source = $("#people_src_title").val();
 		var confidence = $("#people_src_confidence").val();
 		personName.push({source, confidence});
-		console.log(source);
-		console.log(personName);
 	}
 	else {
 		var source = $("#people_src_title").val();
@@ -36,13 +34,51 @@ function createObject() {
 	var notes = $("input[id='people_ae_notes']").val();
 
 	if(personObject != undefined) {
-		personObject = '{"Person_PersonName" : {"values" : ' + name + '}, "sources" : [' + personName + '], "Person_PersonAlias" : {"values" :' + alias + '}, "sources" : [' + personAlias + '], "Person_PersonNotes" : {"values" : ' + notes + '} ' + '}';
+		personObject = {Person_PersonName : {values :  name, sources : personName}, Person_PersonAlias : {values : alias, sources : personAlias}, Person_PersonNotes : {values : notes}};
 	}
 
 	console.log(personObject);
-	console.log(personObject.Person_PersonName.sources + "data");
+	$('#sources_list').empty();
+	createList(personObject);
+}
 
-	for(var i = 0; i < personObject.length; i++) {
+function createList (personObject) {
 
+	for(var i = 0; i < personObject.Person_PersonName.sources.length; i++) {
+		console.log(personObject.Person_PersonName.sources[i]);
+
+		var sourceInfo = '<div class="row clickableRow"><li><p class="col-sm-4">' + personObject.Person_PersonName.sources[i].source + '</p>' +
+		'<div class="form-group dropdown col-sm-7">' +
+		'<select id="people_src_confidence_data">' +
+				'<option>Level of Confidence</option>' +
+				'<option value="">low</option>' +
+				'<option value="">medium</option>' +
+				'<option value="">high</option>' +
+			'</select></div>' +
+		'<div class="col-sm-1 sources_list_remove" id="' + personObject.Person_PersonName.sources[i].source + '"><span title="Delete Source"><i class="fa fa-trash fa-lg"></i></span></div>' +
+		'</li></div>';
+
+		$('#sources_list').append(sourceInfo);
+		$('select').selectpicker('show');
 	}
 }
+
+$(document).on("click", ".sources_list_remove", function (event) {
+    var id = $(this).attr('id');
+
+		console.log(id);
+		delete personObject.Person_PersonName.sources[id];
+		console.log(personObject);
+
+		for(var i = 0; i < personObject.Person_PersonName.sources.length; i++) {
+
+			console.log(personObject.Person_PersonName.sources[i]);
+			console.log(personObject.Person_PersonName.sources[i].source);
+
+			if(personObject.Person_PersonName.sources[i].source === id) {
+				delete personObject.Person_PersonName.sources[i];
+			}
+		}
+
+		console.log(personObject);
+});
