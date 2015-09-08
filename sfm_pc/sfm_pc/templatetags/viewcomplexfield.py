c640c23cbcda23c1ee691000c4e18c0e8ba2c4ec
Guillaume Auger <gauger@caravan.coop>
2015-09-08 11:12:31 -0400

    Add params for view_complex_field templatetag

--- sfm_pc/sfm_pc/templatetags/viewcomplexfield.py
+++ sfm_pc/sfm_pc/templatetags/viewcomplexfield.py
@@ -3,11 +3,14 @@ from django.template import Library
 register = Library()
 
 @register.inclusion_tag('complexfield/view.html')
-def view_complex_field(field, path):
-    field_id = field.table_object.__class__.__name__ + "_" + field.field_model.__name__
+def view_complex_field(field, object_id, path):
+    field_id = field.get_field_str_id()
 
     return {
         'value' : field,
+        'object_id': object_id,
+        'object_name': field.get_object_name(),
+        'attr_name': field.get_attr_name(),
         'field_id': field_id,
         'field_name': field.field_name,
         'sourced': field.sourced,
