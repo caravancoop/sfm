395bc7c567c2bb4a7289827d223384da6b11ca05
lavanyaks <lavanya.singh24@gmail.com>
2015-09-09 17:28:22 -0400

    Merge fix

--- sfm_pc/static/js/mainjs.js
+++ sfm_pc/static/js/mainjs.js
@@ -1,29 +1,17 @@
-var personObject = [],
-personName = [],
-personAlias = [];
+var personObject = [];
 
-var count = 0;
-
-$("a[name='people_ae_name']").on('click', function() {
-	count = 1;
-});
-
-$("a[name='people_ae_alias']").on('click', function() {
-	count = 2;
+$('.modalBox').on('click', function () {
+	var field_str = $(this).data('field-str');
+	var model_id = $(this).data('model-id');
+	console.log(field_str + " " + model_id);
 });
 
 function grabData() {
-	if(count === 1) {
 
-		var source = $("#people_src_title").val();
-		var confidence = $("#people_src_confidence").val();
-		personName.push({source, confidence});
-	}
-	else {
-		var source = $("#people_src_title").val();
-		var confidence = $("#people_src_confidence").val();
-		personAlias.push({source, confidence});
-	}
+	var source = $(".modal").find("src_confidence").val();
+	var confidence = $("#people_src_confidence").val();
+	personName.push({source, confidence});
+
 	createObject();
 }
 
@@ -33,38 +21,48 @@ function createObject() {
 	var alias = $("a").closest("div.options").find("input[id='people_ae_alias']").val();
 	var notes = $("input[id='people_ae_notes']").val();
 
+	var formId = "person";
+
 	if(personObject != undefined) {
-		personObject = {Person_PersonName : {values :  name, sources : personName}, Person_PersonAlias : {values : alias, sources : personAlias}, Person_PersonNotes : {values : notes}};
+		personObject = {
+			Person_PersonName : {
+				values :  name,
+				sources : personName
+			},
+			Person_PersonAlias : {
+				values : alias,
+				sources : personAlias},
+				Person_PersonNotes : {values : notes}
+
+			};
+		}
+
+		console.log(personObject);
+		createList(personObject);
 	}
 
-	console.log(personObject);
-	$('#sources_list').empty();
-	createList(personObject);
-}
+	function createList (personObject) {
 
-function createList (personObject) {
+		for(var i = 0; i < personObject.Person_PersonName.sources.length; i++) {
+			console.log(personObject.Person_PersonName.sources[i]);
 
-	for(var i = 0; i < personObject.Person_PersonName.sources.length; i++) {
-		console.log(personObject.Person_PersonName.sources[i]);
-
-		var sourceInfo = '<div class="row clickableRow"><li><p class="col-sm-4">' + personObject.Person_PersonName.sources[i].source + '</p>' +
-		'<div class="form-group dropdown col-sm-7">' +
-		'<select id="people_src_confidence_data">' +
-				'<option>Level of Confidence</option>' +
-				'<option value="">low</option>' +
-				'<option value="">medium</option>' +
-				'<option value="">high</option>' +
+			var sourceInfo = '<div class="row clickableRow"><li><p class="col-sm-4">' + personObject.Person_PersonName.sources[i].source + '</p>' +
+			'<div class="form-group dropdown col-sm-6">' +
+			'<select id="people_src_confidence_data">' +
+			'<option>Level of Confidence</option>' +
+			'<option value="">low</option>' +
+			'<option value="">medium</option>' +
+			'<option value="">high</option>' +
 			'</select></div>' +
-		'<div class="col-sm-1 sources_list_remove" id="' + personObject.Person_PersonName.sources[i].source + '"><span title="Delete Source"><i class="fa fa-trash fa-lg"></i></span></div>' +
-		'</li></div>';
+			'<div class="col-sm-2 sources_list_remove" id="' + personObject.Person_PersonName.sources[i].source + '"><span title="Delete Source"><i class="fa fa-trash fa-lg"></i></span></div>' +
+			'</li></div>';
 
-		$('#sources_list').append(sourceInfo);
-		$('select').selectpicker('show');
+			$('#sources_list').append(sourceInfo);
+		}
 	}
-}
 
-$(document).on("click", ".sources_list_remove", function (event) {
-    var id = $(this).attr('id');
+	$(document).on("click", ".sources_list_remove", function (event) {
+		var id = $(this).attr('id');
 
 		console.log(id);
 		delete personObject.Person_PersonName.sources[id];
@@ -81,4 +79,4 @@ $(document).on("click", ".sources_list_remove", function (event) {
 		}
 
 		console.log(personObject);
-});
+	});
