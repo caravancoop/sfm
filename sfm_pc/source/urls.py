4f12a0352efe83f4d84ccbe39c74ebf4e7fc2ff8
Guillaume Auger <gauger@caravan.coop>
2015-09-10 14:44:03 -0400

    Add forgoten file

---
+++ sfm_pc/source/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns, url
+
+from source.views import get_sources
+
+urlpatterns = patterns(
+    '',
+    url(r'(?P<object_type>[a-zA-Z]+)/' +
+        '(?P<object_id>[0-9]+)/' +
+        '(?P<field_name>[a-zA-Z]+)/',
+        get_sources,
+        name="get_sources"),
+)
