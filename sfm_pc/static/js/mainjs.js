a31b089371c1a9f46b8fef378f629f4cba00e459
lavanyaks <lavanya.singh24@gmail.com>
2015-09-10 14:02:29 -0400

    Changes after merge - AJAX Call

--- sfm_pc/static/js/mainjs.js
+++ sfm_pc/static/js/mainjs.js
@@ -1,44 +1,42 @@
-var personObject = [],
-personName = [],
-personAlias = [];
+var personObject = [];
 
-var count = 0;
+$('.modalBox').on('click', function () {
+	var person = $('.modalBox').data('field-object-name');
+	var person_id = $('.modalBox').data('field-object-id');
+	var name = $('.modalBox').data('field-attr-name');
 
-$("a[name='people_ae_name']").on('click', function() {
-	count = 1;
-});
+	console.log(person + " " + person_id + " " + name);
 
-$("a[name='people_ae_alias']").on('click', function() {
-	count = 2;
+	$.ajax({
+	type: "GET",
+	url: "/source/" + person + "/" + person_id + "/" + name,
+	contentType: "application/json; charset=utf-8",
+	dataType: "json",
+	crossdomain: true,
+	success: OnGetSourcesSuccess,
+	error: OnGetSourcesError
+	});
 });
 
 function grabData() {
-	if(count === 1) {
 
-		var source = $("#people_src_title").val();
-		var confidence = $("#people_src_confidence").val();
-		personName.push({source, confidence});
-	}
-	else {
-		var source = $("#people_src_title").val();
-		var confidence = $("#people_src_confidence").val();
-		personAlias.push({source, confidence});
-	}
-	createObject();
-}
+	var field_str = $('.modalBox').data('field-str');
+	var model_id = $('.modalBox').data('model-id');
 
-function createObject() {
+	var source = $(".modal-body").find("." + model_id + "_src_addSource").val();
+	var confidence = $(".modal-body").find("." + model_id + "_src_addConfidence").val();
+	console.log(field_str + " " + source + " " + confidence);
 
-	var name = $("a").closest("div.options").find("input[id='people_ae_name']").val();
-	var alias = $("a").closest("div.options").find("input[id='people_ae_alias']").val();
-	var notes = $("input[id='people_ae_notes']").val();
+	personN.push({"sources": [source, confidence]});
 
-	if(personObject != undefined) {
-		personObject = {Person_PersonName : {values :  name, sources : personName}, Person_PersonAlias : {values : alias, sources : personAlias}, Person_PersonNotes : {values : notes}};
+	if(personObject !== undefined) {
+		personObject[field_str] = {
+			values : name,
+			sources : personN
+		}
 	}
 
 	console.log(personObject);
-	$('#sources_list').empty();
 	createList(personObject);
 }
 
@@ -47,38 +45,34 @@ function createList (personObject) {
 	for(var i = 0; i < personObject.Person_PersonName.sources.length; i++) {
 		console.log(personObject.Person_PersonName.sources[i]);
 
-		var sourceInfo = '<div class="row clickableRow"><li><p class="col-sm-4">' + personObject.Person_PersonName.sources[i].source + '</p>' +
-		'<div class="form-group dropdown col-sm-7">' +
-		'<select id="people_src_confidence_data">' +
-				'<option>Level of Confidence</option>' +
-				'<option value="">low</option>' +
-				'<option value="">medium</option>' +
-				'<option value="">high</option>' +
-			'</select></div>' +
-		'<div class="col-sm-1 sources_list_remove" id="' + personObject.Person_PersonName.sources[i].source + '"><span title="Delete Source"><i class="fa fa-trash fa-lg"></i></span></div>' +
+		var sourceInfo = '<div class="row clickableRow"><li><p class="col-sm-4">' + personObject.Person_PersonName.sources[i].sources[0] + '</p>' +
+		'<p class="col-sm-6">' + personObject.Person_PersonName.sources[i].sources[1] + '</p>' +
+		'<div class="col-sm-2 sources_list_remove" id="' + personObject.Person_PersonName.sources[i].sources[0] + '"><span title="Delete Source"><i class="fa fa-trash fa-lg"></i></span></div>' +
 		'</li></div>';
 
 		$('#sources_list').append(sourceInfo);
-		$('select').selectpicker('show');
 	}
 }
 
 $(document).on("click", ".sources_list_remove", function (event) {
-    var id = $(this).attr('id');
 
-		console.log(id);
-		delete personObject.Person_PersonName.sources[id];
-		console.log(personObject);
-
-		for(var i = 0; i < personObject.Person_PersonName.sources.length; i++) {
-
-			console.log(personObject.Person_PersonName.sources[i]);
-			console.log(personObject.Person_PersonName.sources[i].source);
-
-			if(personObject.Person_PersonName.sources[i].source === id) {
-				delete personObject.Person_PersonName.sources[i];
-			}
+	for(var i = 0; i < personObject.Person_PersonName.sources.length; i++) {
+		var id = personObject.Person_PersonName.sources[i].sources[0];
+		if(personObject.Person_PersonName.sources[i].sources[0] === id) {
+			console.log(id);
+			delete personObject.Person_PersonName.sources[i].sources;
+			console.log(personObject);
 		}
-
-		console.log(personObject);
+	}
 });
+
+function OnGetSourcesSuccess(response, status) {
+	var data = response;
+	console.log(response);
+	// release lock
+}
+
+function OnGetSourcesError(request, status, error) {
+	console.log("error:" + error);
+	// release lock
+}
