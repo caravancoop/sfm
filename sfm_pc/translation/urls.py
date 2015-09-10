31dd51db010ab8b2234085fab842582d88c1b2e1
Guillaume Auger <gauger@caravan.coop>
2015-09-10 16:57:54 -0400

    Add language autocomplete endpoint

--- sfm_pc/translation/urls.py
+++ sfm_pc/translation/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, url
 
-from translation.views import translate
+from translation.views import translate, autocomplete_language
 
 urlpatterns = patterns(
     '',
@@ -8,5 +8,8 @@ urlpatterns = patterns(
         '(?P<object_id>[0-9]+)/' +
         '(?P<field_name>[a-zA-Z]+)/',
         translate,
+        name="translate_field"),
+    url(r'languages/autocomplete/',
+        autocomplete_language,
         name="translate_field"),
 )
--- sfm_pc/translation/views.py
+++ sfm_pc/translation/views.py
@@ -3,6 +3,9 @@ import json
 from django.core.exceptions import ValidationError
 from django.views.generic.base import TemplateView
 from django.http import HttpResponse, HttpResponseServerError
+from django.db.models import Q
+
+from languages_plus.models import Language
 
 from complex_fields.models import ComplexFieldContainer
 
@@ -19,3 +22,20 @@ def translate(request, object_type, object_id, field_name):
         return HttpResponseServerError(str(error))
 
     return HttpResponse(status=200)
+
+def autocomplete_language(request):
+    data = request.GET.dict()['term']
+
+    langs = Language.objects.filter(
+        Q(name_en__icontains=data) |
+        Q(name_native__icontains=data) |
+        Q(name_other__icontains=data) |
+        Q(iso_639_1__icontains=data)
+    )
+    import ipdb; ipdb.set_trace()
+    languages = [
+        lang.name_native
+        for lang in langs.all()
+    ]
+
+    return HttpResponse(json.dumps(languages))
