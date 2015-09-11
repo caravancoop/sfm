8ad7beabb787e72b73863c85471fc3377d747d0e
Guillaume Auger <gauger@caravan.coop>
2015-09-11 14:03:19 -0400

    Add allauth: fix login with email

--- sfm_pc/sfm_pc/urls.py
+++ sfm_pc/sfm_pc/urls.py
@@ -26,8 +26,7 @@ urlpatterns = i18n_patterns(
 
     # Ajax calls
     url(r'^ajax/', include('ajax.urls')),
-    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name'\
-        : 'account/login.html'}, name='auth_login'),
+    url(r'^accounts/', include('allauth.urls')),
     url(r'^logout/$', 'django.contrib.auth.views.logout_then_login', {'login_url'\
         : '/login'}),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
