868345929b0f1538f8bde09f621cbb8ae0a0448c
lavanyaks <lavanya.singh24@gmail.com>
2015-09-15 14:31:08 -0400

    JS changes to add/update new person - 0 and object_id

--- sfm_pc/static/js/mainjs.js
+++ sfm_pc/static/js/mainjs.js
@@ -23,7 +23,7 @@ $('.addPerson').on('click', function() {
 	var model_object_id = $('.modalBox').data('model-object-id');
 	console.log("model_object_id: " + model_object_id);
 
-	if(model_object_id === "None") {
+	if(model_object_id == 0) {
 		model_object_id = "add";
 	}
 
@@ -74,7 +74,27 @@ function removeListElements () {
 	$('#sources_list > li').remove();
 }
 
+$('#complexFieldModal').on('shown.bs.modal', function () {
+
+	$('select').selectpicker('refresh');
+
+	object_name = $('.modal-header').data('field-object-name');
+	object_id = $('.modal-header').data('field-object-id');
+	field_name = $('.modal-header').data('field-attr-name');
+	modal_type = $('.modal-header').data('modal-type');
+	var getURL = "/" + modal_type + "/" + object_name + "/" + object_id + "/" + field_name;
+
+	console.log(modal_type);
+	console.log(object_name + " " + object_id + " " + field_name);
+
+	genericGetFunction(object_name, object_id, field_name, modal_type, getURL);
+
+});
+
+//Genric AJAX call to get info for the modals
 function genericGetFunction (object_name, object_id, field_name, modal_type, getURL) {
+
+	console.log(object_id);
 
 	if (modal_type === "version") {
 		var language = document.getElementById('people_vr_language').value;
@@ -99,27 +119,27 @@ function genericGetFunction (object_name, object_id, field_name, modal_type, get
 	});
 }
 
-
+//Remove sources from the source modal
 $(document).on("click", ".sources_list_remove", function (event) {
 	var id = $(this).attr('id');
 
 	for(var i = 0; i < genericObject.length; i++) {
-
 		if(genericObject[i].source === id) {
 			genericObject.splice(i, 1);
 		}
 	}
 
+//Remove all elements from the existing list and create a new one.
 	removeListElements();
 	createSourcesList(genericObject);
 });
 
 function separateObjects(response, modal_type) {
-
+//Setting the content of genericObject to an empty object
 	genericObject = [];
 
 	for (var i in response) {
-		console.log(response[i]);
+		// console.log(response[i]);
 		genericObject.push(response[i]);
 	}
 
@@ -144,9 +164,8 @@ function createSourcesList (genericObject) {
 
 	for(var i = 0; i < genericObject.length; i++) {
 
-		var sourceInfo = genericObject[i];
-
 		var confidenceString;
+		var sourceInfo = genericObject[i];
 
 		switch(sourceInfo.confidence) {
 
@@ -181,7 +200,7 @@ function createVersionsList (genericObject) {
 
 	$('#versions_list').find("tr:not(:has(th))").remove();
 
-	// Get a reference to the sources list in the main DOM.
+	// Get a reference to the versions list in the main DOM.
 	var versionsList = document.getElementById('versions_list');
 
 	for(var i = 0; i < genericObject.length; i++) {
@@ -191,11 +210,10 @@ function createVersionsList (genericObject) {
 		var versiontempl = document.getElementById('version_template').content.cloneNode(true);
 
 		versiontempl.querySelector('.version').textContent = versionInfo.value;
-		console.log(genericObject[i]);
+		// console.log(genericObject[i]);
 		for (var j = 0; j < genericObject[i].sources.length; j++) {
 
 			var confidenceString;
-			// console.log(genericObject[i].sources[j].confidence);
 
 			switch(genericObject[i].sources[j].confidence) {
 
@@ -216,10 +234,10 @@ function createVersionsList (genericObject) {
 
 			}
 
-			versionSources += genericObject[i].sources[j].source + " " + confidenceString + String.fromCharCode(10);
+			versionSources += genericObject[i].sources[j].source + " - " + confidenceString + String.fromCharCode(10);
 			versiontempl.querySelector('#versions_sub_list').textContent = versionSources;
 
-			console.log(versionSources);
+			// console.log(versionSources);
 		}
 		versionSources = null;
 		versiontempl.querySelector('.revertButton').value = versionInfo.id;
@@ -231,13 +249,12 @@ function createTranslationsList (genericObject) {
 
 	$('#translate_table').find("tr:not(:has(th))").remove();
 
-	// Get a reference to the sources list in the main DOM.
+	// Get a reference to the translation list in the main DOM.
 	var translateList = document.getElementById('translate_table');
 
 	for(var i = 0; i < genericObject.length; i++) {
 
 		var translateInfo = genericObject[i];
-
 		var tmpl = document.getElementById('translate_template').content.cloneNode(true);
 
 		tmpl.querySelector('.tr_language').textContent = translateInfo.lang;
@@ -246,23 +263,6 @@ function createTranslationsList (genericObject) {
 		translateList.appendChild(tmpl);
 	}
 }
-
-$('#complexFieldModal').on('shown.bs.modal', function () {
-
-	$('select').selectpicker('refresh');
-
-	object_name = $('.modal-header').data('field-object-name');
-	object_id = $('.modal-header').data('field-object-id');
-	field_name = $('.modal-header').data('field-attr-name');
-	modal_type = $('.modal-header').data('modal-type');
-	var getURL = "/" + modal_type + "/" + object_name + "/" + object_id + "/" + field_name;
-
-	console.log(modal_type);
-	console.log(object_name + " " + object_id + " " + field_name);
-
-	genericGetFunction(object_name, object_id, field_name, modal_type, getURL);
-
-});
 
 function changeLanguage () {
 
@@ -319,13 +319,14 @@ function getTranslation () {
 		dataType: "json",
 		crossdomain: true,
 		success: function (response, status) {
-			console.log(response + "getTranslation");
+			console.log(response + "get Translation");
 			separateObjects(response, "translate");
 		},
 		error: function (request, status, error) {
 			console.log(error + "error in Translation");
 		}
 	});
+
 }
 
 function addTranslation () {
@@ -340,7 +341,6 @@ function addTranslation () {
 	$.ajax({
 		type: "POST",
 		url: "/" + window.LANG + "/translate/" + object_name + "/" + object_id + "/" + field_name + "/",
-		// dataType: 'json',
 		data: {
 			csrfmiddlewaretoken: window.CSRF_TOKEN,
 			translation: JSON.stringify(data)
