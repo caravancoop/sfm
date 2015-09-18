12f6887adcedcf0211a2bc538e257b2a6fdf5bfb
Guillaume Auger <gauger@caravan.coop>
2015-09-18 11:17:16 -0400

    Add empty dashboard template

--- sfm_pc/sfm_pc/views.py
+++ sfm_pc/sfm_pc/views.py
@@ -2,7 +2,7 @@ from django.views.generic.base import TemplateView
 
 
 class Dashboard(TemplateView):
-    template_name = 'sfm_pc/dashboard.html'
+    template_name = 'sfm/dashboard.html'
 
     def get_context_data(self, **kwargs):
         context = super(Dashboard, self).get_context_data(**kwargs)
