659787179c1e21018bc0cdf0536bf0a83ebdd9dd
Guillaume Auger <gauger@caravan.coop>
2015-09-18 10:18:44 -0400

    Add minimal organization

--- sfm_pc/organization/urls.py
+++ sfm_pc/organization/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import patterns, url
 
-from .views import OrganizationCreate, OrganizationView, classification_autocomplete
+from .views import (OrganizationCreate, OrganizationView, OrganizationUpdate,
+                    classification_autocomplete)
 
 urlpatterns = patterns(
     '',
     url(r'^$', OrganizationView.as_view(), name='organizations'),
     url(r'add/$', OrganizationCreate.as_view(), name="add_organization"),
-
+    url(r'(?P<pk>\d+)/$', OrganizationUpdate.as_view(), name='edit_organization'),
     url(r'classification/autocomplete', classification_autocomplete,
         name="classification_autocomplete")
-)
