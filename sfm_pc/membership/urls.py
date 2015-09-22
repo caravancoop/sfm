1ff06ad6533473b5542eb1532d053a59098fbff7
Guillaume Auger <gauger@caravan.coop>
2015-09-22 14:13:15 -0400

    Add membership search page

--- sfm_pc/membership/urls.py
+++ sfm_pc/membership/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, url
 
-from .views import MembershipCreate
+from .views import MembershipCreate, MembershipView
 
 urlpatterns = patterns(
     '',
+    url(r'^$', MembershipView.as_view(), name='membership'),
     url(r'add/$', MembershipCreate.as_view(), name="add_membership"),
 )
--- sfm_pc/membership/views.py
+++ sfm_pc/membership/views.py
@@ -4,9 +4,43 @@ from django.views.generic.edit import UpdateView
 from django.views.generic.base import TemplateView
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db.models import Max
 
 from organization.models import Organization
-from .models import Membership
+from .models import Membership, Role, Rank
+
+
+class MembershipView(TemplateView):
+    template_name = 'membership/search.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(MembershipView, self).get_context_data(**kwargs)
+
+        order_by = self.request.GET.get('orderby')
+        if not order_by:
+            order_by = 'membershipperson__value'
+
+        direction = self.request.GET.get('direction')
+        if not direction:
+            direction = 'ASC'
+
+        dirsym = ''
+        if direction == 'DESC':
+            dirsym = '-'
+
+        membership_query = (Membership.objects
+                        .annotate(Max(order_by))
+                        .order_by(dirsym + order_by + "__max"))
+
+        role = self.request.GET.get('role')
+        if role:
+            org_query = membership_query.filter(membershiprole__id=role)
+
+        context['memberships'] = membership_query
+        context['roles'] = Role.objects.all()
+        context['ranks'] = Rank.objects.all()
+
+        return context
 
 
 class MembershipCreate(TemplateView):
@@ -22,5 +56,7 @@ class MembershipCreate(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(MembershipCreate, self).get_context_data(**kwargs)
         context['membership'] = Membership()
+        context['roles'] = Role.objects.all()
+        context['ranks'] = Rank.objects.all()
 
         return context
