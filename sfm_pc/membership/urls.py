056310ce1b3395efa791136651b6666bafe81f23
Guillaume Auger <gauger@caravan.coop>
2015-09-22 18:01:31 -0400

    Add membership update endpoint

--- sfm_pc/membership/urls.py
+++ sfm_pc/membership/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, url
 
-from .views import MembershipCreate, MembershipView
+from .views import MembershipView, MembershipCreate, MembershipUpdate
 
 urlpatterns = patterns(
     '',
     url(r'^$', MembershipView.as_view(), name='membership'),
     url(r'add/$', MembershipCreate.as_view(), name="add_membership"),
+    url(r'(?P<pk>\d+)/$', MembershipUpdate.as_view(), name='edit_person'),
 )
