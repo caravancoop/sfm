f957a1e7ca6bd9f270b6a5009b44810b8dd01b73
Guillaume Auger <gauger@caravan.coop>
2015-09-25 11:56:56 -0400

    Fix optional argument: field_id

--- sfm_pc/version/views.py
+++ sfm_pc/version/views.py
@@ -5,7 +5,7 @@ from django.utils.translation import get_language
 
 from complex_fields.models import ComplexFieldContainer
 
-def get_versions(request, object_type, object_id, field_name, field_id,
+def get_versions(request, object_type, object_id, field_name, field_id=None,
                  lang=get_language()):
     field = ComplexFieldContainer.field_from_str_and_id(
         object_type, object_id, field_name, field_id
