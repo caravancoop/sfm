bdb3e21a6826915b6c73149dc3ecbff079ca744d
stayflydieyoung <franknwankwo@gmail.com>
2015-09-30 14:43:29 -0400

    deathdate in progress

--- sfm_pc/static/js/module-controller.js
+++ sfm_pc/static/js/module-controller.js
@@ -3,14 +3,37 @@ var moduleController = (function(){
     init:function(){
       this.cacheDom();
       this.getModules();
+      this.setDates();
       // this.bindEvents();
     },
     cacheDom:function(){
       this.$el_modal =  $('#complexFieldModal');
-      this._srcModule = null;
-      this._versModule = null;
-      this._langModule = null;
+      this.dateWrapper = $('.date-wrapper');
+      // this._srcModule = null;
+      // this._versModule = null;
+      // this._langModule = null;
+
     },
+    setDates:function(){
+
+      var d = new Date();
+      var day, month, year, currYear, currMonth, currDay;
+      var firstYear = 1950;
+      currYear = d.getFullYear();
+      var self = this;
+      this.dateWrapper.each(function(){
+
+        year = self.dateWrapper.find('.-sfm-year'); // set the year
+        month = self.dateWrapper.find('.-sfm-month'); // set the month
+        day = self.dateWrapper.find('.-sfm-day'); // set the date
+        for(var y = firstYear; y <= currYear; ++y){
+          $(year).append('<option value=' + y + '>' + y + '</option>');
+        }
+      });
+      $('select').selectpicker('refresh');
+
+    },
+
     // this function binds all the initial events to the selectors
     bindEvents:function(){
       // $('body').on('click', '.addPerson', this.update.bind(this));
@@ -18,13 +41,16 @@ var moduleController = (function(){
     getModules:function(){
       this._srcModule = source.publicAPI();
       this._langModule = language.publicAPI();
-    }
+    },
+    daysInMonth:function (month,year) {
+      return new Date(year, month, 0).getDate();
+    },
   };
   modCtrl.init();
   return {
     dispModObjs: function(){
-      console.log(modCtrl._srcModule);
-      console.log(modCtrl._langModule);
+      // console.log(modCtrl._srcModule);
+      // console.log(modCtrl._langModule);
     }
   };
 })();
