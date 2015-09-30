ea7411094601170289e9a38099f593f2d4ce22e5
Guillaume Auger <gauger@caravan.coop>
2015-09-30 10:30:34 -0400

    Fix composition model, remove some organization remaining

--- sfm_pc/composition/models.py
+++ sfm_pc/composition/models.py
@@ -12,45 +12,46 @@ from complex_fields.models import ComplexField, ComplexFieldContainer
 
 class Composition(models.Model):
     def __init__(self, *args, **kwargs):
-        self.parent = ComplexFieldContainer(self, OrganizationCompositionParent)
-        self.child = ComplexFieldContainer(self, OrganizationCompositionChild)
-        self.startdate = ComplexFieldContainer(self, OrganizationCompositionStartDate)
-        self.enddate = ComplexFieldContainer(self, OrganizationCompositionEndDate)
-        self.classification = ComplexFieldContainer(self, OrganizationCompositionClassification)
+        super().__init__(*args, **kwargs)
+        self.parent = ComplexFieldContainer(self, CompositionParent)
+        self.child = ComplexFieldContainer(self, CompositionChild)
+        self.startdate = ComplexFieldContainer(self, CompositionStartDate)
+        self.enddate = ComplexFieldContainer(self, CompositionEndDate)
+        self.classification = ComplexFieldContainer(self, CompositionClassification)
 
 
 @versioned
 @sourced
 class CompositionParent(ComplexField):
-    object_ref = models.ForeignKey(Organization)
+    object_ref = models.ForeignKey(Composition)
     value = models.ForeignKey(Organization, related_name='child_organization')
-    field_name = _("Child organization")
-
-@versioned
-@sourced
-class CompositionChild(ComplexField):
-    object_ref = models.ForeignKey(Organization)
-    value = models.ForeignKey(Organization, related_name='parent_organization')
     field_name = _("Parent organization")
 
 @versioned
 @sourced
+class CompositionChild(ComplexField):
+    object_ref = models.ForeignKey(Composition)
+    value = models.ForeignKey(Organization, related_name='parent_organization')
+    field_name = _("Child organization")
+
+@versioned
+@sourced
 class CompositionStartDate(ComplexField):
-    object_ref = models.ForeignKey(Organization)
+    object_ref = models.ForeignKey(Composition)
     value = ApproximateDateField(default=None, blank=True, null=True)
     field_name = _("Start date")
 
 @versioned
 @sourced
 class CompositionEndDate(ComplexField):
-    object_ref = models.ForeignKey(Organization)
+    object_ref = models.ForeignKey(Composition)
     value = ApproximateDateField(default=None, blank=True, null=True)
     field_name = _("End date")
 
 @versioned
 @sourced
 class CompositionClassification(ComplexField):
-    object_ref = models.ForeignKey(Organization)
+    object_ref = models.ForeignKey(Composition)
     value = models.ForeignKey(Classification, default=None, blank=True,
                               null=True)
     field_name = _("Classification")
