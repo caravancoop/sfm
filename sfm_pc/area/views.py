60697f4f086248e17e12e74a5495cd0aa659b6d9
Guillaume Auger <gauger@caravan.coop>
2015-10-06 09:57:50 -0400

    Fix area create endpoint: 'area' key to 'object'

--- sfm_pc/area/views.py
+++ sfm_pc/area/views.py
@@ -4,14 +4,16 @@ from django.views.generic.base import TemplateView
 from django.http import HttpResponse
 
 from .models import Area
+from .forms import ZoneForm
 
-# Create your views her
+
+
 class AreaCreate(TemplateView):
     template_name = 'area/edit.html'
 
     def post(self, request, *args, **kwargs):
         context = self.get_context_data()
-        data = json.loads(request.POST.dict()['area'])
+        data = json.loads(request.POST.dict()['object'])
         area = Area.create(data)
 
         return HttpResponse(json.dumps({"success": True}), content_type="application/json")
@@ -19,5 +21,7 @@ class AreaCreate(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(AreaCreate, self).get_context_data(**kwargs)
         context['area'] = Area()
+        context['zone'] = ZoneForm()
+
 
         return context
