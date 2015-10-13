3e99f17cb660e1a153dd7edec52d313e661ad7da
Guillaume Auger <gauger@caravan.coop>
2015-10-13 11:25:26 -0400

    Add urls for organization search

--- sfm_pc/organization/urls.py
+++ sfm_pc/organization/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import patterns, url
 
 from .views import (OrganizationCreate, OrganizationView, OrganizationUpdate,
-                    classification_autocomplete, organization_autocomplete)
+                    classification_autocomplete, organization_autocomplete,
+                    organization_search)
 
 urlpatterns = patterns(
     '',
     url(r'^$', OrganizationView.as_view(), name='organizations'),
+    url(r'search/', organization_search, name='organization_search'),
     url(r'add/$', OrganizationCreate.as_view(), name="add_organization"),
     url(r'(?P<pk>\d+)/$', OrganizationUpdate.as_view(), name='edit_organization'),
     url(r'classification/autocomplete', classification_autocomplete,
