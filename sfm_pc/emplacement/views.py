9ae07c2e36612cec860f2bdde727f5d28f764e27
Guillaume Auger <gauger@caravan.coop>
2015-10-14 11:46:11 -0400

    Add emplacement base views

---
+++ sfm_pc/emplacement/views.py
@@ -0,0 +1,68 @@
+from datetime import date
+
+import json
+
+from django.views.generic.base import TemplateView
+from django.http import HttpResponse
+
+from .models import Emplacement
+
+
+class EmplacementView(TemplateView):
+    template_name = 'emplacement/search.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(EmplacementView, self).get_context_data(**kwargs)
+
+        context['year_range'] = range(1955, date.today().year + 1)
+        context['day_range'] = range(1, 31)
+
+        return context
+
+class EmplacementUpdate(TemplateView):
+    template_name = 'emplacement/edit.html'
+
+    def post(self, request, *args, **kwargs):
+        data = json.loads(request.POST.dict()['object'])
+        try:
+            emplacement = Emplacement.objects.get(pk=kwargs.get('pk'))
+        except Emplacement.DoesNotExist:
+            msg = "This emplacement does not exist, it should be created " \
+                  "before updating it."
+            return HttpResponse(msg, status=400)
+
+        errors = emplacement.update(data)
+        if errors is None:
+            return HttpResponse(
+                json.dumps({"success": True}),
+                content_type="application/json"
+            )
+        else:
+            return HttpResponse(
+                json.dumps({"success": False, "errors": errors}),
+                content_type="application/json"
+            )
+
+    def get_context_data(self, **kwargs):
+        context = super(EmplacementUpdate, self).get_context_data(**kwargs)
+        emplacement = Emplacement.objects.get(pk=context.get('pk'))
+        context['emplacement'] = emplacement
+
+        return context
+
+class EmplacementCreate(TemplateView):
+    template_name = 'emplacement/edit.html'
+
+    def post(self, request, *args, **kwargs):
+        context = self.get_context_data()
+        data = json.loads(request.POST.dict()['object'])
+        emplacement = Emplacement.create(data)
+
+        return HttpResponse(json.dumps({"success": True}), content_type="application/json")
+
+    def get_context_data(self, **kwargs):
+        context = super(EmplacementCreate, self).get_context_data(**kwargs)
+        context['emplacement'] = Emplacement()
+
+        return context
+
