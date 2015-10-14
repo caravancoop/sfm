440a8d41c20829ceb2dc487906f9b5364b61bdc4
Guillaume Auger <gauger@caravan.coop>
2015-10-14 11:51:50 -0400

    Add emplacement admin.py

---
+++ sfm_pc/emplacement/admin.py
@@ -0,0 +1,27 @@
+import reversion
+
+from django.contrib import admin
+
+from .models import (Emplacement, EmplacementStartDate, EmplacementEndDate,
+                     EmplacementOrganization, EmplacementSite)
+
+class EmplacementAdmin(admin.ModelAdmin):
+    pass
+
+class EmplacementStartDateAdmin(reversion.VersionAdmin):
+    pass
+
+class EmplacementEndDateAdmin(reversion.VersionAdmin):
+    pass
+
+class EmplacementOrganizationAdmin(reversion.VersionAdmin):
+    pass
+
+class EmplacementSiteAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Emplacement, EmplacementAdmin)
+admin.site.register(EmplacementStartDate, EmplacementStartDateAdmin)
+admin.site.register(EmplacementEndDate, EmplacementEndDateAdmin)
+admin.site.register(EmplacementOrganization, EmplacementOrganizationAdmin)
+admin.site.register(EmplacementSite, EmplacementSiteAdmin)
