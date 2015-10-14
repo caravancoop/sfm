7fc64a50a7e6ac7848ee181a6804df3cba4d436b
Guillaume Auger <gauger@caravan.coop>
2015-10-14 11:24:13 -0400

    Add site base views

--- sfm_pc/site_sfm/views.py
+++ sfm_pc/site_sfm/views.py
@@ -1,3 +1,62 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.views.generic.base import TemplateView
+from django.http import HttpResponse
+
+from .models import Site
+
+
+class SiteView(TemplateView):
+    template_name = 'site/search.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(SiteView, self).get_context_data(**kwargs)
+
+
+class SiteUpdate(TemplateView):
+    template_name = 'site/edit.html'
+
+    def post(self, request, *args, **kwargs):
+        data = json.loads(request.POST.dict()['object'])
+        try:
+            site = Site.objects.get(pk=kwargs.get('pk'))
+        except Site.DoesNotExist:
+            msg = "This site does not exist, it should be created " \
+                  "before updating it."
+            return HttpResponse(msg, status=400)
+
+        errors = site.update(data)
+        if errors is None:
+            return HttpResponse(
+                json.dumps({"success": True}),
+                content_type="application/json"
+            )
+        else:
+            return HttpResponse(
+                json.dumps({"success": False, "errors": errors}),
+                content_type="application/json"
+            )
+
+    def get_context_data(self, **kwargs):
+        context = super(SiteUpdate, self).get_context_data(**kwargs)
+        site = Site.objects.get(pk=context.get('pk'))
+        context['site'] = site
+
+        return context
+
+class SiteCreate(TemplateView):
+    template_name = 'site/edit.html'
+
+    def post(self, request, *args, **kwargs):
+        context = self.get_context_data()
+        data = json.loads(request.POST.dict()['object'])
+        site = Site.create(data)
+
+        return HttpResponse(json.dumps({"success": True}), content_type="application/json")
+
+    def get_context_data(self, **kwargs):
+        context = super(SiteCreate, self).get_context_data(**kwargs)
+        context['site'] = Site()
+
+
+        return context
