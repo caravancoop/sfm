8f66fb90755a0238c0bef1f59b2285d1d19685dc
Guillaume Auger <gauger@caravan.coop>
2015-10-14 12:01:17 -0400

    Add site admin

--- sfm_pc/site_sfm/admin.py
+++ sfm_pc/site_sfm/admin.py
@@ -1,3 +1,35 @@
+import reversion
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import (Site, SiteName, SiteAdminLevel1, SiteAdminLevel2, SiteCoordinates,
+                     SiteGeoname, SiteGeonameId)
+
+class SiteAdmin(admin.ModelAdmin):
+    pass
+
+class SiteNameAdmin(reversion.VersionAdmin):
+    pass
+
+class SiteAdminLevel1Admin(reversion.VersionAdmin):
+    pass
+
+class SiteAdminLevel2Admin(reversion.VersionAdmin):
+    pass
+
+class SiteCoordinatesAdmin(reversion.VersionAdmin):
+    pass
+
+class SiteGeonameAdmin(reversion.VersionAdmin):
+    pass
+
+class SiteGeonameIdAdmin(reversion.VersionAdmin):
+    pass
+
+admin.site.register(Site, SiteAdmin)
+admin.site.register(SiteName, SiteNameAdmin)
+admin.site.register(SiteAdminLevel1, SiteAdminLevel1Admin)
+admin.site.register(SiteAdminLevel2, SiteAdminLevel2Admin)
+admin.site.register(SiteCoordinates, SiteCoordinatesAdmin)
+admin.site.register(SiteGeoname, SiteGeonameAdmin)
+admin.site.register(SiteGeonameId, SiteGeonameIdAdmin)
