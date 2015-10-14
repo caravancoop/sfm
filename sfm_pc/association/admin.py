4d08c35a3b80991e8691cfe58be9ebcfd597a82e
Guillaume Auger <gauger@caravan.coop>
2015-10-14 10:02:10 -0400

    Add association admin

--- sfm_pc/association/admin.py
+++ sfm_pc/association/admin.py
@@ -1,3 +1,27 @@
+import reversion
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import (Association, AssociationStartDate, AssociationEndDate,
+                     AssociationOrganization, AssociationArea)
+
+class AssociationAdmin(admin.ModelAdmin):
+    pass
+
+class AssociationStartDateAdmin(reversion.VersionAdmin):
+    pass
+
+class AssociationEndDateAdmin(reversion.VersionAdmin):
+    pass
+
+class AssociationOrganizationAdmin(reversion.VersionAdmin):
+    pass
+
+class AssociationAreaAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Association, AssociationAdmin)
+admin.site.register(AssociationStartDate, AssociationStartDateAdmin)
+admin.site.register(AssociationEndDate, AssociationEndDateAdmin)
+admin.site.register(AssociationOrganization, AssociationOrganizationAdmin)
+admin.site.register(AssociationArea, AssociationAreaAdmin)
