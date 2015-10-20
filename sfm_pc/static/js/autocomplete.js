97beb4cda9f7f2350428d65e33eb0d5aa04680de
Guillaume Auger <gauger@caravan.coop>
2015-10-20 17:01:56 -0400

    Add autocomplete templatetag

---
+++ sfm_pc/static/js/autocomplete.js
@@ -0,0 +1,12 @@
+$(document).on("keydown.autocomplete", ".autocomplete-tag", function() {
+    self = this;
+    $(this).autocomplete({
+        source: $(this).data('source-url'),
+        select: function(event, ui) {
+            event.preventDefault();
+            $(self).val(ui.item.label);
+            input_id = $(self).data('id');
+            $(self).parent().find('#' + input_id).val(ui.item.value);
+        }
+    });
+});
