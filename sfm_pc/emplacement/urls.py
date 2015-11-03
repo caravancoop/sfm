ab8d47325e1dbd14038898e917da49da217b6ea1
Guillaume Auger <gauger@caravan.coop>
2015-11-03 14:32:19 -0500

    Add list to emplacement search

--- sfm_pc/emplacement/urls.py
+++ sfm_pc/emplacement/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, url
 
-from .views import EmplacementCreate, EmplacementUpdate, EmplacementView
+from .views import (EmplacementCreate, EmplacementUpdate, EmplacementView,
+                    emplacement_search)
 
 urlpatterns = patterns(
     '',
     url(r'add/$', EmplacementCreate.as_view(), name='add_emplacement'),
+    url(r'search/', emplacement_search, name='emplacement_search'),
     url(r'(?P<pk>\d+)/$', EmplacementUpdate.as_view(), name='edit_emplacement'),
     url(r'^$', EmplacementView.as_view(), name='emplacement'),
 )
--- sfm_pc/emplacement/views.py
+++ sfm_pc/emplacement/views.py
@@ -2,7 +2,10 @@ from datetime import date
 
 import json
 
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.views.generic.base import TemplateView
+from django.utils.translation import ugettext as _
+from django.template.loader import render_to_string
 from django.http import HttpResponse
 
 from .models import Emplacement
@@ -18,6 +21,54 @@ class EmplacementView(TemplateView):
         context['day_range'] = range(1, 31)
 
         return context
+
+
+def emplacement_search(request):
+    terms = request.GET.dict()
+
+    emplacement_query = Emplacement.objects.all()
+    page = int(terms.get('page', 1))
+
+    column_names = [_('Start date'), _('End date'), _('Organization'), _('Site')]
+    keys = ['startdate', 'enddate', 'organization', 'site']
+
+    paginator = Paginator(emplacement_query, 15)
+    try:
+        emplacement_page = paginator.page(page)
+    except PageNotAnInteger:
+        person_page = paginator.page(1)
+        page = 1
+    except EmptyPage:
+        person_page = paginator.page(paginator.num_pages)
+        page = paginator.num_pages
+
+    emplacements = [
+        {
+            "id": emplacement.id,
+            "organization": str(emplacement.organization.get_value()),
+            "site": str(emplacement.site.get_value()),
+            "startdate": str(emplacement.startdate.get_value()),
+            "enddate": str(emplacement.enddate.get_value()),
+        }
+        for emplacement in emplacement_page
+    ]
+
+    html_paginator = render_to_string(
+        'paginator.html',
+        {'actual': page, 'min': page - 5, 'max': page + 5,
+         'paginator': emplacement_page,
+         'pages': range(1, paginator.num_pages + 1)}
+    )
+
+    return HttpResponse(json.dumps({
+        'success': True,
+        'column_names': column_names,
+        'keys': keys,
+        'objects': emplacements,
+        'paginator': html_paginator,
+        'result_number': len(emplacement_query)
+    }))
+
 
 class EmplacementUpdate(TemplateView):
     template_name = 'emplacement/edit.html'
