02bf362e1b4d2d1630f4794f96456fc6282e579a
Guillaume Auger <gauger@caravan.coop>
2015-11-03 15:00:25 -0500

    Add list to geosite search

--- sfm_pc/geosite/urls.py
+++ sfm_pc/geosite/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, url
 
-from .views import SiteCreate, SiteUpdate, SiteView, site_autocomplete
+from .views import SiteCreate, SiteUpdate, SiteView, site_search, site_autocomplete
 
 urlpatterns = patterns(
     '',
     url(r'add/$', SiteCreate.as_view(), name='add_site'),
+    url(r'search/', site_search, name='site_search'),
     url(r'(?P<pk>\d+)/$', SiteUpdate.as_view(), name='edit_site'),
     url(r'^$', SiteView.as_view(), name='site'),
     url(r'autocomplete', site_autocomplete,
--- sfm_pc/geosite/views.py
+++ sfm_pc/geosite/views.py
@@ -1,7 +1,9 @@
 import json
 
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.views.generic.base import TemplateView
 from django.utils.translation import ugettext as _
+from django.template.loader import render_to_string
 from django.http import HttpResponse
 
 from .forms import ZoneForm
@@ -13,6 +15,51 @@ class SiteView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(SiteView, self).get_context_data(**kwargs)
+
+def site_search(request):
+    terms = request.GET.dict()
+
+    geosite_query = Geosite.objects.all()
+    page = int(terms.get('page', 1))
+
+    column_names = [_('Name'), _('GeoName'), _('GeoName ID')]
+    keys = ['name', 'geoname', 'geonameid']
+
+    paginator = Paginator(geosite_query, 15)
+    try:
+        geosite_page = paginator.page(page)
+    except PageNotAnInteger:
+        person_page = paginator.page(1)
+        page = 1
+    except EmptyPage:
+        person_page = paginator.page(paginator.num_pages)
+        page = paginator.num_pages
+
+    geosites = [
+        {
+            "id": geosite.id,
+            "name": str(geosite.name.get_value()),
+            "geoname": str(geosite.geoname.get_value()),
+            "geonameid": str(geosite.geonameid.get_value()),
+        }
+        for geosite in geosite_page
+    ]
+
+    html_paginator = render_to_string(
+        'paginator.html',
+        {'actual': page, 'min': page - 5, 'max': page + 5,
+         'paginator': geosite_page,
+         'pages': range(1, paginator.num_pages + 1)}
+    )
+
+    return HttpResponse(json.dumps({
+        'success': True,
+        'column_names': column_names,
+        'keys': keys,
+        'objects': geosites,
+        'paginator': html_paginator,
+        'result_number': len(geosite_query)
+    }))
 
 
 class SiteUpdate(TemplateView):
