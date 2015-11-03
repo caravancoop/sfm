1454b7dec80238fa91b1f4050c08fc3323e05bf7
Guillaume Auger <gauger@caravan.coop>
2015-11-03 14:07:49 -0500

    Add list in area search

--- sfm_pc/association/urls.py
+++ sfm_pc/association/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, url
 
-from .views import AssociationCreate, AssociationUpdate, AssociationView
+from .views import (AssociationCreate, AssociationUpdate, AssociationView,
+                    association_search)
 
 urlpatterns = patterns(
     '',
     url(r'add/$', AssociationCreate.as_view(), name='add_association'),
+    url(r'search/', association_search, name='association_search'),
     url(r'(?P<pk>\d+)/$', AssociationUpdate.as_view(), name='edit_association'),
     url(r'^$', AssociationView.as_view(), name='association'),
 )
