c8d66759b140a25fda7dfbeeb2d8d1ff1c6ee363
Guillaume Auger <gauger@caravan.coop>
2015-11-03 15:20:02 -0500

    Add list to violation search

--- sfm_pc/violation/urls.py
+++ sfm_pc/violation/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, url
 
-from .views import ViolationCreate, ViolationUpdate, ViolationView
+from .views import ViolationCreate, ViolationUpdate, ViolationView, violation_search
 
 urlpatterns = patterns(
     '',
     url(r'add/$', ViolationCreate.as_view(), name='add_violation'),
+    url(r'search/', violation_search, name='violation_search'),
     url(r'(?P<pk>\d+)/$', ViolationUpdate.as_view(), name='edit_violation'),
     url(r'^$', ViolationView.as_view(), name='violation'),
 )
--- sfm_pc/violation/views.py
+++ sfm_pc/violation/views.py
@@ -1,6 +1,9 @@
 import json
 
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.views.generic.base import TemplateView
+from django.utils.translation import ugettext as _
+from django.template.loader import render_to_string
 from django.http import HttpResponse
 
 from .models import Violation, Type
@@ -13,6 +16,55 @@ class ViolationView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(ViolationView, self).get_context_data(**kwargs)
+
+
+def violation_search(request):
+    terms = request.GET.dict()
+
+    violation_query = Violation.objects.all()
+    page = int(terms.get('page', 1))
+
+    column_names = [_('Start date'), _('End date'), _('GeoName'),
+                     _('Perpetrator'), _('Organization')]
+    keys = ['startdate', 'enddate', 'geoname', 'perpetrator', 'organization']
+
+    paginator = Paginator(violation_query, 15)
+    try:
+        violation_page = paginator.page(page)
+    except PageNotAnInteger:
+        person_page = paginator.page(1)
+        page = 1
+    except EmptyPage:
+        person_page = paginator.page(paginator.num_pages)
+        page = paginator.num_pages
+
+    violations = [
+        {
+            "id": violation.id,
+            "startdate": str(violation.startdate.get_value()),
+            "enddate": str(violation.enddate.get_value()),
+            "geoname": str(violation.geoname.get_value()),
+            "perpetrator": str(violation.perpetrator.get_value()),
+            "organization": str(violation.perpetratororganization.get_value()),
+        }
+        for violation in violation_page
+    ]
+
+    html_paginator = render_to_string(
+        'paginator.html',
+        {'actual': page, 'min': page - 5, 'max': page + 5,
+         'paginator': violation_page,
+         'pages': range(1, paginator.num_pages + 1)}
+    )
+
+    return HttpResponse(json.dumps({
+        'success': True,
+        'column_names': column_names,
+        'keys': keys,
+        'objects': violations,
+        'paginator': html_paginator,
+        'result_number': len(violation_query)
+    }))
 
 
 class ViolationUpdate(TemplateView):
