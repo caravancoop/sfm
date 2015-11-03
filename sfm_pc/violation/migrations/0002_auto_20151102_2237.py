1cbfe1d21a7e3e6cfe1026acc7c5c0ce7fa0ab96
Guillaume Auger <gauger@caravan.coop>
2015-11-03 08:50:15 -0500

    Add violation migrations: confidence levels choices

---
+++ sfm_pc/violation/migrations/0002_auto_20151102_2237.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('source', '0001_initial'),
+        ('violation', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='violationlocationdescription',
+            name='confidence',
+            field=models.CharField(default=1, choices=[('1', 'Low'), ('2', 'Medium'), ('3', 'High')], max_length=1),
+        ),
+        migrations.AddField(
+            model_name='violationlocationdescription',
+            name='lang',
+            field=models.CharField(null=True, max_length=5),
+        ),
+        migrations.AddField(
+            model_name='violationlocationdescription',
+            name='sources',
+            field=models.ManyToManyField(related_name='violation_violationlocationdescription_related', to='source.Source'),
+        ),
+    ]
