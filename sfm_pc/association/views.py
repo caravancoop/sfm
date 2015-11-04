4804cc05d1abee4f84520c377299756c60739dff
Guillaume Auger <gauger@caravan.coop>
2015-11-04 15:05:53 -0500

    Add search for association

--- sfm_pc/association/views.py
+++ sfm_pc/association/views.py
@@ -7,6 +7,7 @@ from django.views.generic.base import TemplateView
 from django.utils.translation import ugettext as _
 from django.template.loader import render_to_string
 from django.http import HttpResponse
+from django.db.models import Max
 
 from .models import Association
 
@@ -26,8 +27,73 @@ class AssociationView(TemplateView):
 def association_search(request):
     terms = request.GET.dict()
 
-    association_query = Association.objects.all()
+    order_by = terms.get('orderby')
+    if not order_by:
+        order_by = 'associationstartdate__value'
+    elif order_by in ['startdate']:
+        order_by = 'association' + order_by + '__value'
+
+    direction = terms.get('direction')
+    if not direction:
+        direction = 'ASC'
+
+    dirsym = ''
+    if direction == 'DESC':
+        dirsym = '-'
+
+    association_query = (Association.objects
+                         .annotate(Max(order_by))
+                         .order_by(dirsym + order_by + "__max"))
+
     page = int(terms.get('page', 1))
+
+    startdate_year = terms.get('startdate_year')
+    if startdate_year:
+        association_query = association_query.filter(
+            associationstartdate__value__startswith=startdate_year
+        )
+
+    startdate_month = terms.get('startdate_month')
+    if startdate_month:
+        association_query = association_query.filter(
+            associationstartdate__value__contains="-" + startdate_month + "-"
+        )
+
+    startdate_day = terms.get('startdate_day')
+    if startdate_day:
+        association_query = association_query.filter(
+            associationstartdate__value__endswith=startdate_day
+        )
+
+    enddate_year = terms.get('enddate_year')
+    if enddate_year:
+        association_query = association_query.filter(
+            associationenddate__value__startswith=enddate_year
+        )
+
+    enddate_month = terms.get('enddate_month')
+    if enddate_month:
+        association_query = association_query.filter(
+            associationenddate__value__contains="-" + enddate_month + "-"
+        )
+
+    enddate_day = terms.get('enddate_day')
+    if enddate_day:
+        association_query = association_query.filter(
+            associationenddate__value__endswith=enddate_day
+        )
+
+    organization = terms.get('organization')
+    if organization:
+        association_query = association_query.filter(
+            associationorganization__value__organizationname__value__icontains=organization
+        )
+
+    area = terms.get('area')
+    if area:
+        association_query = association_query.filter(
+            associationarea__value__areaname__value__icontains=area
+        )
 
     column_names = [_('Start date'), _('End date'), _('Organization'), _('Area')]
     keys = ['startdate', 'enddate', 'organization', 'area']
