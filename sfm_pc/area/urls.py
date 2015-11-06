5c54b1c6124116f68b7f47e371042057f5a2b5c5
Guillaume Auger <gauger@caravan.coop>
2015-11-06 16:31:01 -0500

    Add area delete

--- sfm_pc/area/urls.py
+++ sfm_pc/area/urls.py
@@ -1,13 +1,16 @@
 from django.conf.urls import patterns, url
 
 from .views import (AreaCreate, AreaUpdate, AreaView, area_search,
-                    area_autocomplete, code_autocomplete, area_csv)
+                    area_autocomplete, code_autocomplete, area_csv, AreaDelete)
 
 urlpatterns = patterns(
     '',
     url(r'add/$', AreaCreate.as_view(), name='add_area'),
     url(r'search/', area_search, name='area_search'),
     url(r'csv/', area_csv, name='area_csv'),
+    url(r'delete/(?P<pk>\d+)/$',
+        AreaDelete.as_view(success_url="/area/"),
+        name='delete_area'),
     url(r'(?P<pk>\d+)/$', AreaUpdate.as_view(), name='edit_area'),
     url(r'^$', AreaView.as_view(), name='area'),
     url(r'code/autocomplete', code_autocomplete,
