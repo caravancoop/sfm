f36c12b5979f7c2ed0fd03bf3937aa89c8b79167
Guillaume Auger <gauger@caravan.coop>
2015-11-06 16:17:41 -0500

    Add association delete

--- sfm_pc/association/urls.py
+++ sfm_pc/association/urls.py
@@ -1,13 +1,16 @@
 from django.conf.urls import patterns, url
 
 from .views import (AssociationCreate, AssociationUpdate, AssociationView,
-                    association_search, association_csv)
+                    association_search, association_csv, AssociationDelete)
 
 urlpatterns = patterns(
     '',
     url(r'add/$', AssociationCreate.as_view(), name='add_association'),
     url(r'search/', association_search, name='association_search'),
     url(r'csv/', association_csv, name='association_csv'),
+    url(r'delete/(?P<pk>\d+)/$',
+        AssociationDelete.as_view(success_url="/association/"),
+        name='delete_association'),
     url(r'(?P<pk>\d+)/$', AssociationUpdate.as_view(), name='edit_association'),
     url(r'^$', AssociationView.as_view(), name='association'),
 )
