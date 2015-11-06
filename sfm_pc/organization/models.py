e6b6850c21a32214b3a99ad79c8e35d0f1bba75f
Guillaume Auger <gauger@caravan.coop>
2015-11-06 15:21:56 -0500

    Refact organization search: in model

--- sfm_pc/organization/models.py
+++ sfm_pc/organization/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.translation import ugettext as _
 from django.utils.translation import get_language
+from django.db.models import Max
 
 from django_date_extensions.fields import ApproximateDateField
 
@@ -35,6 +36,100 @@ class Organization(models.Model, BaseModel):
 
     def __str__(self):
         return self.name.get_value()
+
+    @classmethod
+    def search(cls, terms):
+        order_by = terms.get('orderby')
+        if not order_by:
+            order_by = 'organizationname__value'
+
+        direction = terms.get('direction')
+        if not direction:
+            direction = 'ASC'
+
+        dirsym = ''
+        if direction == 'DESC':
+            dirsym = '-'
+
+        orgs_query = (Organization.objects
+                      .annotate(Max(order_by))
+                      .order_by(dirsym + order_by + "__max"))
+
+        name = terms.get('name')
+        if name:
+            orgs_query = orgs_query.filter(organizationname__value__icontains=name)
+
+        alias_val = terms.get('alias')
+        if alias_val:
+            orgs_query = orgs_query.filter(organizationalias__value__icontains=alias_val)
+
+        foundingdate_year = terms.get('founding_year')
+        if foundingdate_year:
+            orgs_query = orgs_query.filter(
+                organizationfoundingdate__value__startswith=foundingdate_year
+            )
+
+        foundingdate_month = terms.get('founding_month')
+        if foundingdate_month:
+            orgs_query = orgs_query.filter(
+                organizationfoundingdate__value__contains="-" + foundingdate_month + "-"
+            )
+
+        foundingdate_day = terms.get('founding_day')
+        if foundingdate_day:
+            orgs_query = orgs_query.filter(
+                organizationfoundingdate__value__endswith=foundingdate_day
+            )
+
+        dissolutiondate_year = terms.get('dissolution_year')
+        if dissolutiondate_year:
+            orgs_query = orgs_query.filter(
+                organizationdissolutiondate__value__startswith=dissolutiondate_year
+            )
+
+        dissolutiondate_month = terms.get('dissolution_month')
+        if dissolutiondate_month:
+            orgs_query = orgs_query.filter(
+                organizationdissolutiondate__value__contains="-" + dissolutiondate_month + "-"
+            )
+
+        dissolutiondate_day = terms.get('dissolution_day')
+        if dissolutiondate_day:
+            orgs_query = orgs_query.filter(
+                organizationdissolutiondate__value__endswith=dissolutiondate_day
+            )
+
+        classification = terms.get('classification')
+        if classification:
+            orgs_query = orgs_query.filter(organizationclassification__value_id=classification)
+
+        latitude = terms.get('latitude')
+        longitude = terms.get('longitude')
+        if latitude and longitude:
+            try:
+                latitude = float(latitude)
+                longitude = float(longitude)
+            except ValueError:
+                latitude = 0
+                longitude = 0
+
+            point = geos.Point(latitude, longitude)
+            radius = terms.get('radius')
+            if radius:
+                try:
+                    radius = float(radius)
+                except ValueError:
+                    radius = 0
+                orgs_query = orgs_query.filter(
+                    associationorganization__object_ref__associationarea__value__areageometry__value__dwithin=(point, radius)
+                )
+            else:
+                orgs_query = orgs_query.filter(
+                    associationorganization__object_ref__associationarea__value__areageometry__value__bbcontains=point
+                )
+
+        return orgs_query
+
 
 @translated
 @versioned
