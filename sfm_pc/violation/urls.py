53e63726af0a4abf239e4df9344880c0fa90279f
Guillaume Auger <gauger@caravan.coop>
2015-11-06 17:29:15 -0500

    Add violation delete

--- sfm_pc/violation/urls.py
+++ sfm_pc/violation/urls.py
@@ -1,13 +1,16 @@
 from django.conf.urls import patterns, url
 
 from .views import (ViolationCreate, ViolationUpdate, ViolationView, violation_search,
-                    violation_csv)
+                    violation_csv, ViolationDelete)
 
 urlpatterns = patterns(
     '',
     url(r'add/$', ViolationCreate.as_view(), name='add_violation'),
     url(r'search/', violation_search, name='violation_search'),
     url(r'csv/', violation_csv, name='violation_csv'),
+    url(r'delete/(?P<pk>\d+)/$',
+        ViolationDelete.as_view(success_url="/violation/"),
+        name='delete_violation'),
     url(r'(?P<pk>\d+)/$', ViolationUpdate.as_view(), name='edit_violation'),
     url(r'^$', ViolationView.as_view(), name='violation'),
 )
--- sfm_pc/violation/views.py
+++ sfm_pc/violation/views.py
@@ -3,14 +3,37 @@ import csv
 from datetime import date
 
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.contrib.admin.util import NestedObjects
+from django.views.generic.edit import DeleteView
 from django.views.generic.base import TemplateView
 from django.utils.translation import ugettext as _
 from django.template.loader import render_to_string
 from django.http import HttpResponse
+from django.db import DEFAULT_DB_ALIAS
 
 from .models import Violation, Type
 from source.models import Source
 from .forms import ZoneForm
+from sfm_pc.utils import deleted_in_str
+
+
+class ViolationDelete(DeleteView):
+    model = Violation
+    template_name = "delete_confirm.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(ViolationDelete, self).get_context_data(**kwargs)
+        collector = NestedObjects(using=DEFAULT_DB_ALIAS)
+        collector.collect([context['object']])
+        deleted_elements = collector.nested()
+        context['deleted_elements'] = deleted_in_str(deleted_elements)
+        return context
+
+
+    def get_object(self, queryset=None):
+        obj = super(ViolationDelete, self).get_object()
+
+        return obj
 
 
 class ViolationView(TemplateView):
