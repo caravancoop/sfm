605a126cff2b8e3aa2a2417d85e5063ca7112993
Guillaume Auger <gauger@caravan.coop>
2015-11-06 15:46:00 -0500

    Refact: move composition search to model

--- sfm_pc/composition/views.py
+++ sfm_pc/composition/views.py
@@ -71,79 +71,9 @@ class CompositionView(TemplateView):
 def composition_search(request):
     terms = request.GET.dict()
 
-    order_by = terms.get('orderby')
-    if not order_by:
-        order_by = 'compositionstartdate__value'
-    elif order_by in ['startdate']:
-        order_by = 'composition' + order_by + '__value'
-
-    direction = terms.get('direction')
-    if not direction:
-        direction = 'ASC'
-
-    dirsym = ''
-    if direction == 'DESC':
-        dirsym = '-'
-
-    composition_query = (Composition.objects
-                    .annotate(Max(order_by))
-                    .order_by(dirsym + order_by + "__max"))
+    composition_query = Composition.search(terms)
 
     page = int(terms.get('page', 1))
-
-    startdate_year = terms.get('startdate_year')
-    if startdate_year:
-        composition_query = composition_query.filter(
-            compositionstartdate__value__startswith=startdate_year
-        )
-
-    startdate_month = terms.get('startdate_month')
-    if startdate_month:
-        composition_query = composition_query.filter(
-            compositionstartdate__value__contains="-" + startdate_month + "-"
-        )
-
-    startdate_day = terms.get('startdate_day')
-    if startdate_day:
-        composition_query = composition_query.filter(
-            compositionstartdate__value__endswith=startdate_day
-        )
-
-    enddate_year = terms.get('enddate_year')
-    if enddate_year:
-        composition_query = composition_query.filter(
-            compositionenddate__value__startswith=enddate_year
-        )
-
-    enddate_month = terms.get('enddate_month')
-    if enddate_month:
-        composition_query = composition_query.filter(
-            compositionenddate__value__contains="-" + enddate_month + "-"
-        )
-
-    enddate_day = terms.get('enddate_day')
-    if enddate_day:
-        composition_query = composition_query.filter(
-            compositionenddate__value__endswith=enddate_day
-        )
-
-    classification = terms.get('classification')
-    if classification:
-        composition_query = composition_query.filter(
-            compositionclassification__value_id=classification
-        )
-
-    parent = terms.get('parent')
-    if parent:
-        composition_query = composition_query.filter(
-            compositionparent__value__organizationname__value__icontains=parent
-        )
-
-    child = terms.get('child')
-    if child:
-        composition_query = composition_query.filter(
-            compositionchild__value__organizationname__value__icontains=child
-        )
 
     keys = ['parent', 'child', 'classification', 'startdate', 'enddate']
 
