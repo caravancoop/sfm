c4943a260ed34d814534d6cb8bd8f888a8ed6004
James McKinney <james@slashpoundbang.com>
2016-02-09 20:23:19 -0500

    Remove unused variable

--- sfm_pc/sfm_pc/urls.py
+++ sfm_pc/sfm_pc/urls.py
@@ -1,6 +1,6 @@
 
 from django.conf import settings
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
