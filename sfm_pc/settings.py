710d70526d39873c27fdba8543d03b9c2af22afe
James McKinney <james@slashpoundbang.com>
2016-02-09 21:52:09 -0500

    Set STATIC_ROOT

--- sfm_pc/settings.py
+++ sfm_pc/settings.py
@@ -143,7 +143,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = ''
+STATIC_ROOT = 'staticfiles'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
