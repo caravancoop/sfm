4777c4a67db184b68cb5a256c40aad4f1a9c83ac
Eric van Zanten <eric.vanzanten@gmail.com>
2016-07-29 17:00:34 -0500

    Trying to parse source strings

--- sfm_pc/management/commands/import_google_doc.py
+++ sfm_pc/management/commands/import_google_doc.py
@@ -7,9 +7,21 @@ import httplib2
 from oauth2client.service_account import ServiceAccountCredentials
 from apiclient.discovery import build
 
+import datefinder
+
 from django.core.management.base import BaseCommand, CommandError
 
+from source.models import Source, Publication
+from organization.models import Organization, OrganizationAlias, \
+    OrganizationClassification
+
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+
+CONFIDENCE_MAP = {
+    'Low': 1,
+    'Medium': 2,
+    'High': 3,
+}
 
 class Command(BaseCommand):
     help = 'Import data from Google Drive Spreadsheet'
@@ -18,7 +30,7 @@ class Command(BaseCommand):
         parser.add_argument(
             '--doc_id',
             dest='doc_id',
-            default='1Jom4qjn3RnEGGxpBNqMIeEBZC7M8JiOl2g1sPTCiBas',
+            default='16cRBkrnXE5iGm8JXD7LSqbeFOg_anhVp2YAzYTRYDgU',
             help='Import data from specified Google Drive Document'
         )
     
@@ -76,8 +88,31 @@ class Command(BaseCommand):
 
         self.stdout.write(self.style.SUCCESS('Successfully imported data from {}'.format(options['doc_id'])))
 
+    def create_source(self, source_string):
+        
+        # TODO: Maybe finding index of date string in source will give
+        # us orientation to find other parts
+        for dates, indices in datefinder.find_dates(source_string, index=True):
+            print(dates, indicies)
+        return source
+
     def create_organization(self, org_dict):
+        # Name, Aliases, Classification
+        
         name = org_dict['Name']
+        name_confidence = CONFIDENCE_MAP[org_dict['Confidence: Name']]
+        name_source = self.create_source(org_dict['Source: Name'])
+
+        # aliases = org_dict['Aliases or alternative spellings (semi-colon separated)'].split(';')
+        # classification = org_dict['Classification'].split(';')
+        # 
+        # org_info = {
+        #     'Organization_OrganizationName': {
+        #         'value': name,
+        #         'confidence': org_dict['Confidence: Name']
+        #     }
+        # }
+
 
     def create_person(self, person_dict):
         pass
