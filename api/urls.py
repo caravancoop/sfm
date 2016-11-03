50720180c6b6dc1733f3309cbf0f6746dfab1b8a
Eric van Zanten <eric.vanzanten@gmail.com>
2016-11-03 16:57:53 -0500

    Adding people api

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 
 from api.views import CountryListView, CountryDetailView, CountryZipView, \
     CountryTxtView, CountryMapView, CountryEventsView, EventDetailView, \
-    GeonameAutoView, OrganizationSearchView
+    GeonameAutoView, OrganizationSearchView, PeopleSearchView
 
 urlpatterns = [
     url(r'^countries/$', CountryListView.as_view(), name="country-list"),
@@ -13,6 +13,7 @@ urlpatterns = [
     url(r'^countries/(?P<id>\w+)/events/$', CountryEventsView.as_view(), name="country-events"),
     url(r'^countries/(?P<id>\w+)/autocomplete/geonames_id/$', GeonameAutoView.as_view(), name="geoname-auto"),
     url(r'^countries/(?P<id>\w+)/search/organizations/$', OrganizationSearchView.as_view(), name="organization-search"),
+    url(r'^countries/(?P<id>\w+)/search/people/$', PeopleSearchView.as_view(), name="people-search"),
     url(r'^events/(?P<id>\S+)/$', EventDetailView.as_view(), name="event-detail"),
     
 ]
--- api/views.py
+++ api/views.py
@@ -1,317 +1,8 @@
 from collections import OrderedDict
-import itertools
-import json
-from datetime import datetime
 
-from django.http import JsonResponse, HttpResponse
-from django.views.generic import TemplateView
 from django.db import connection
 
-from violation.models import Violation
-from sfm_pc.utils import get_org_hierarchy_by_id
-
-OPERATOR_LOOKUP = {
-    'lte': '<=',
-    'gte': '>=',
-    'eq': '=',
-}
-
-class ValidationError(Exception):
-    def __init__(self, message):
-        self.message = message
-
-def dateValidator(value):
-    try:
-        datetime.strptime(value, '%Y-%m-%d')
-    except ValueError:
-        raise ValidationError('Incorrect date format. Should be YYYY-MM-DD.')
-
-def integerValidator(value):
-    try:
-        int(value)
-    except ValueError:
-        raise ValidationError('{} is not an integer'.format(value))
-
-class JSONResponseMixin(object):
-    def render_to_json_response(self, context, **response_kwargs):
-        return JsonResponse(
-            self.get_data(context),
-            **response_kwargs
-        )
-
-    def get_data(self, context):
-        try:
-            del context['view']
-        except (KeyError, TypeError):
-            pass
-        # Make sure things are JSON serializable
-        return context
-
-class JSONAPIView(JSONResponseMixin, TemplateView):
-    safe = True
-    
-    def dispatch(self, request, *args, **kwargs):
-        errors = []
-        self.wheres = []
-        self.order_bys = []
-
-        filter_fields = list(getattr(self, 'filter_fields', {}).keys())
-        order_by_fields = list(getattr(self, 'order_by_fields', {}).keys())
-        required_params = getattr(self, 'required_params', [])
-        optional_params = getattr(self, 'optional_params', [])
-
-        valid_params = required_params + filter_fields + order_by_fields + optional_params
-        
-        incoming_params = list(self.request.GET.keys())
-        
-        missing = set(required_params) - set(incoming_params)
-
-        if missing:
-            if len(missing) > 1:
-                errors.append("{} are required fields".format(', '.join(missing)))
-            else:
-                errors.append('{} is a required field'.format(list(missing)[0]))
-
-        for query_param in self.request.GET.keys():
-            try:
-                field, operator = query_param.split('__')
-            except ValueError:
-                field = query_param
-                operator = 'eq'
-
-            if field not in valid_params:
-                errors.append("'{}' is not a valid query parameter".format(query_param))
-            
-            elif not self.checkOperator(field, operator):
-                errors.append("Invalid operator for '{1}'".format(operator, field))
-
-            elif field in filter_fields:
-                
-                value = self.request.GET[query_param]
-                
-                validator = self.filter_fields[field]['validator']
-                
-                if validator:
-                    try:
-                        validator(value)
-                        self.wheres.append((field, operator, value,))
-                    except ValidationError as e:
-                        errors.append("Value for '{0}' is not valid: {1}".format(field, e.message))
-            
-            elif field == 'o':
-                value = self.request.GET['o']
-                if value not in order_by_fields:
-                    errors.append("Cannot order by '{}'".format(value))
-                else:
-                    sort_order = 'ASC'
-                    
-                    if value.startswith('-'):
-                        sort_order = 'DESC'
-                        value = value.strip('-')
-
-                    self.order_bys.append((value, sort_order,))
-
-            elif field == 'p':
-                value = self.request.GET['p']
-                try:
-                    int(value)
-                except ValueError:
-                    errors.append("'{}' is not a valid page number")
-        
-        if errors:
-            response = HttpResponse(json.dumps({'errors': errors}), 
-                                    content_type='application/json')
-            response.status_code = 400
-        else:
-            response = super().dispatch(request, *args, **kwargs)
-
-        return response
-    
-    def checkOperator(self, field, operator):
-        if field in self.filter_fields:
-            if operator not in self.filter_fields[field]['operators']:
-                return False
-        elif operator != 'eq':
-            return False
-        
-        return True
-
-    def render_to_response(self, context, **response_kwargs):
-        response_kwargs.update({'safe': self.safe})
-        return self.render_to_json_response(context, **response_kwargs)
-        
-    def makeFeature(self, geometry, properties):
-        
-        try:
-            del properties['location']
-        except KeyError:
-            pass
-        
-        feature = {
-            'type': 'Feature',
-            'id': properties['id'],
-            'properties': properties,
-            'geometry': geometry
-        }
-
-        return feature
-    
-    def makeOrganization(self, properties, relationships=True):
-        
-        if relationships:
-            hierarchy = get_org_hierarchy_by_id(properties['id'])
-            
-            properties['root_name'] = None
-            properties['root_id'] = None
-
-            if hierarchy:
-                top = hierarchy[-1]
-                properties['root_name'] = top.name
-                properties['root_id'] = top.id
-
-        event_count = ''' 
-            SELECT COUNT(*) AS count
-            FROM violation
-            WHERE perpetrator_organization_id = %s
-        '''
-
-        cursor = connection.cursor()
-        cursor.execute(event_count, [properties['id']])
-        
-        properties['event_count'] = cursor.fetchone()[0]
-        
-        current_commander = ''' 
-            SELECT DISTINCT ON (o.id)
-              o.id,
-              p.name,
-              m.first_cited,
-              m.last_cited
-            FROM organization AS o
-            JOIN membershipperson AS m
-              ON o.id = m.organization_id
-            JOIN person AS p
-              ON m.member_id = p.id
-            WHERE o.id = %s
-            ORDER BY o.id, 
-                     m.last_cited DESC, 
-                     m.first_cited DESC
-        '''
-        
-        cursor = connection.cursor()
-        cursor.execute(current_commander, [properties['id']])
-        columns = [c[0] for c in cursor.description]
-        
-        row = cursor.fetchone()
-        
-        if row:
-            properties['current_commander'] = dict(zip(columns, row))
-        else:
-            properties['current_commander'] = {}
-        
-        site_present = ''' 
-            SELECT DISTINCT ON (o.id)
-              o.id,
-              g.name AS location,
-              g.admin_level_1 AS admin_level_1_geonames_name,
-              g.geoname AS geonames_name,
-              g.first_cited AS date_first_cited,
-              g.last_cited AS date_last_cited
-            FROM organization AS o
-            JOIN emplacement AS e
-              ON o.id = e.organization_id
-            JOIN geosite AS g
-              ON e.site_id = g.id
-            WHERE o.id = %s
-            ORDER BY o.id, 
-                     g.last_cited DESC, 
-                     g.first_cited DESC
-        '''
-        
-        cursor = connection.cursor()
-        cursor.execute(site_present, [properties['id']])
-        columns = [c[0] for c in cursor.description]
-        
-        row = cursor.fetchone()
-        
-        if row:
-            properties['site_current'] = dict(zip(columns, row))
-        else:
-            properties['site_current'] = {}
-        
-        area_present = ''' 
-            SELECT DISTINCT ON (o.id)
-              o.id,
-              a.name,
-              a.geoname AS geonames_name,
-              a.geonameid,
-              a.geometry
-            FROM organization AS o
-            JOIN association AS ass
-              ON o.id = ass.organization_id
-            JOIN area AS a
-              ON ass.area_id = a.id
-            WHERE o.id = %s
-            ORDER BY o.id, 
-                     a.last_cited DESC, 
-                     a.first_cited DESC
-        '''
-        
-        cursor = connection.cursor()
-        cursor.execute(area_present, [properties['id']])
-        columns = [c[0] for c in cursor.description]
-        
-        row = cursor.fetchone()
-        
-        if row:
-            properties['area_current'] = dict(zip(columns, row))
-        else:
-            properties['area_current'] = {}
-
-        return properties
-
-    def makeEvent(self, properties):
-        properties['classification'] = list(set(properties['classification']))
-
-        perp_class = [c for c in list(set(properties['perpetrator_classification'])) if c]
-        if perp_class:
-            properties['perpetrator_classification'] = perp_class
-        else:
-            properties['perpetrator_classification'] = None
-
-        perp_org_ids = []
-        perp_orgs = []
-        for org in properties['perpetrator_organization']:
-            if org and org['id'] not in perp_org_ids:
-                structured_org = self.makeOrganization(org, relationships=False)
-                
-                perp_orgs.append(structured_org)
-                perp_org_ids.append(org['id'])
-
-        properties['perpetrator_organization'] = perp_orgs
-        return properties
-    
-    def makeWhereClauses(self):
-        where_clauses = []
-
-        for field, operator, value in self.wheres:
-            operator = OPERATOR_LOOKUP[operator]
-            
-            if self.filter_fields.get(field):
-                db_field = self.filter_fields[field]['field']
-            
-                clause = '{0} {1} %s'.format(db_field, operator)
-                where_clauses.append((clause, value,))
-        
-        return where_clauses
-        
-    def makeOrderBy(self):
-        order_bys = []
-
-        for field, sort_order in self.order_bys:
-            db_field = self.order_by_fields[field]['field']
-            order_bys.append('{0} {1}'.format(db_field, sort_order))
-
-        return order_bys
+from api.base_views import JSONAPIView, dateValidator, integerValidator
 
 class OrganizationSearchView(JSONAPIView):
     
@@ -338,11 +29,11 @@ class OrganizationSearchView(JSONAPIView):
             'operators': ['eq'],
             'validator': integerValidator,
         },
-        'events_count': {
-            'field': 'events_count',
-            'operators': ['gte', 'lte',],
-            'validator': integerValidator,
-        },
+        # 'events_count': {
+        #     'field': 'events_count',
+        #     'operators': ['gte', 'lte',],
+        #     'validator': integerValidator,
+        # },
     }
     
     order_by_fields = {
@@ -359,39 +50,8 @@ class OrganizationSearchView(JSONAPIView):
     required_params = ['q']
     optional_params = ['o', 'p']
 
-    def appendWhereClauses(self, query, query_args):
-        where_clauses = self.makeWhereClauses()
-        
-        for clause, value in where_clauses:
-            query = '{0} AND {1}'.format(query, clause)
-            query_args.append(value)
-        
-        return query, query_args
-
-    def getFacets(self, base_query, query_args):
-        
-        facets_counts = '''
-            SELECT 
-              o.classification AS facet,
-              COUNT(*) AS facet_count
-            {}
-        '''.format(base_query)
-        
-        facets_counts, query_args = self.appendWhereClauses(facets_counts, query_args)
-
-        facets_counts = '{} GROUP BY o.classification'.format(facets_counts)
-        
-        cursor = connection.cursor()
-        cursor.execute(facets_counts, query_args)
-        
-        facets_counts = [r for r in cursor]
-        
-        count = sum(f[1] for f in facets_counts)
-        
-        return count, facets_counts
-    
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+        context = {}
         
         country_code = kwargs['id']
         division_id = 'ocd-division/country:{}'.format(country_code)
@@ -415,7 +75,16 @@ class OrganizationSearchView(JSONAPIView):
               AND o.division_id = %s
         '''
         
-        count, facets_counts = self.getFacets(base_query, query_args)
+        facets_counts = '''
+            SELECT 
+              TRIM(o.classification) AS facet,
+              COUNT(*) AS facet_count
+            {}
+        '''.format(base_query)
+        
+        count, facets_counts = self.getFacets(facets_counts, 
+                                              query_args, 
+                                              'classification')
         
         context['count'] = count
         context['facets'] = facets_counts
@@ -426,7 +95,7 @@ class OrganizationSearchView(JSONAPIView):
             SELECT 
               o.id, 
               MAX(o.name) AS name,
-              array_agg(DISTINCT o.alias) AS other_names,
+              array_agg(DISTINCT TRIM(o.alias)) AS other_names,
               ST_AsGeoJSON(MAX(g.coordinates))::json AS location,
               MAX(e.start_date) AS start_date,
               MAX(e.end_date) AS end_date
@@ -462,6 +131,124 @@ class OrganizationSearchView(JSONAPIView):
 
         return context
 
+class PeopleSearchView(JSONAPIView):
+    
+    page_count = 20
+
+    filter_fields = {
+        'date_first_cited': {
+            'field': 'start_date', 
+            'operators': ['gte', 'lte'],
+            'validator': dateValidator,
+        },
+        'date_last_cited': {
+            'field': 'end_date',
+            'operators': ['gte', 'lte'],
+            'validator': dateValidator,
+        },
+        'rank': {
+            'field': 'rank',
+            'operators': ['in'],
+            'validator': None,
+        },
+        'role': {
+            'field': 'role',
+            'operators': ['in'],
+            'validator': None,
+        },
+        'geonames_id': {
+            'field': 'geoname_id',
+            'operators': ['eq'],
+            'validator': integerValidator,
+        },
+        # 'events_count': {
+        #     'field': 'events_count',
+        #     'operators': ['gte', 'lte',],
+        #     'validator': integerValidator,
+        # },
+    }
+    
+    order_by_fields = {
+        'name': {'field': 'name'},
+        # 'events_count': {},
+        '-name': {'field': 'name'},
+        # '-events_count',
+    }
+    
+    required_params = ['q']
+    optional_params = ['o', 'p']
+
+    def get_context_data(self, **kwargs):
+        context = {}
+        
+        country_code = kwargs['id']
+        division_id = 'ocd-division/country:{}'.format(country_code)
+        
+        query_term = self.request.GET['q']
+        
+        query_args = [query_term, division_id]
+        
+        people = '''
+            SELECT 
+              id, name, other_names, event_count, current_membership[1]
+            FROM (
+            SELECT
+              p.id,
+              MAX(p.name) AS name,
+              array_agg(DISTINCT TRIM(p.alias)) AS other_names,
+              COUNT(v.*) AS event_count,
+              array_agg(row_to_json(cm.*)) AS current_membership
+            FROM person AS p
+            JOIN person_person AS pp
+              ON p.id = pp.uuid
+            JOIN search_index AS si
+              ON pp.id = si.id
+            LEFT JOIN (
+              SELECT DISTINCT ON (member_id) 
+                mp.member_id,
+                row_to_json(o.*) AS organization,
+                mp.role,
+                mp.rank
+              FROM membershipperson AS mp
+              JOIN (
+                SELECT DISTINCT ON (o.id)
+                  o.*,
+                  json_build_object('type', 'Feature', 
+                                    'id', g.id, 
+                                    'properties', json_build_object('location', g.name, 
+                                                                    'geonames_name', g.geoname,
+                                                                    'admin_level_1_geonames_name', g.admin_level_1),
+                                    'geometry', ST_AsGeoJSON(g.coordinates)::json) AS site_present
+                  FROM organization AS o
+                  JOIN emplacement AS e
+                    ON o.id = e.organization_id
+                  JOIN geosite AS g
+                    ON e.site_id = g.id
+                  ORDER BY o.id, e.end_date DESC, e.start_date DESC
+              ) AS o
+                ON mp.organization_id = o.id
+              ORDER BY member_id, 
+                       last_cited DESC, 
+                       first_cited DESC
+            ) AS cm
+              ON p.id = cm.member_id
+            LEFT JOIN violation AS v
+              ON p.id = v.perpetrator_id
+            WHERE si.content_type = 'Person'
+              AND plainto_tsquery('english', %s) @@ si.content
+              AND p.division_id = %s
+            GROUP BY p.id
+            ) AS s
+        '''
+        
+        cursor = connection.cursor()
+        cursor.execute(people, query_args)
+        columns = [c[0] for c in cursor.description]
+        
+        context['results'] = [OrderedDict(zip(columns, r)) for r in cursor]
+
+        return context
+
 class GeonameAutoView(JSONAPIView):
     
     safe = False
@@ -478,7 +265,6 @@ class GeonameAutoView(JSONAPIView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         
-        # TODO: Add some error handling for poorly formatted requests
         q = self.request.GET['q']
         classification = self.request.GET.get('classification')
         bbox = self.request.GET.get('bbox')
@@ -556,7 +342,6 @@ class CountryMapView(JSONAPIView):
         country_code = kwargs['id']
         division_id = 'ocd-division/country:{}'.format(country_code)
         
-        # TODO: Add some error handling for poorly formatted requests
         when = self.request.GET['at']
         bbox = self.request.GET.get('bbox')
         classification = self.request.GET.get('classification__in')
