ec52e76adbcc76f54ac7207988c5cc54f8d34d6a
Eric van Zanten <eric.vanzanten@gmail.com>
2017-03-02 15:43:43 -0600

    Add sources to areas and sites

--- api/base_views.py
+++ api/base_views.py
@@ -417,7 +417,6 @@ class JSONAPIView(JSONResponseMixin, TemplateView, CacheMixin):
                 'first_cited': row[7],
                 'last_cited': row[8],
                 'sources': [],
-                'confidence': row[10]
             }
 
             source_ids = []
@@ -426,6 +425,9 @@ class JSONAPIView(JSONResponseMixin, TemplateView, CacheMixin):
                 if source['id'] not in source_ids:
                     member['sources'].append(source)
                     source_ids.append(source['id'])
+            
+            if row[10]:
+                member['confidence'] = REVERSE_CONFIDENCE[int(row[10])].title()
 
             members.append(member)
 
@@ -542,7 +544,6 @@ class JSONAPIView(JSONResponseMixin, TemplateView, CacheMixin):
     def makeOrganizationGeographies(self,
                                     properties,
                                     when,
-                                    all_geography=False,
                                     tolerance=0.001):
 
         site_present = '''
@@ -601,14 +602,11 @@ class JSONAPIView(JSONResponseMixin, TemplateView, CacheMixin):
         cursor.execute(site_present, q_params)
         columns = [c[0] for c in cursor.description]
 
-        if all_geography:
-            properties['sites'] = [self.makeFeature(r[-1], dict(zip(columns, r))) for r in cursor]
+        row = cursor.fetchone()
+        if row:
+            properties['site_current'] = dict(zip(columns, row))
         else:
-            row = cursor.fetchone()
-            if row:
-                properties['site_current'] = dict(zip(columns, row))
-            else:
-                properties['site_current'] = {}
+            properties['site_current'] = {}
 
         area_present = '''
             SELECT DISTINCT ON (ass.id)
@@ -666,16 +664,101 @@ class JSONAPIView(JSONResponseMixin, TemplateView, CacheMixin):
         cursor.execute(area_present, q_params)
         columns = [c[0] for c in cursor.description]
 
-        if all_geography:
-            properties['areas'] = [self.makeFeature(r[-1], dict(zip(columns, r))) for r in cursor]
+        row = cursor.fetchone()
+
+        if row:
+            properties['area_current'] = dict(zip(columns, row))
         else:
-            row = cursor.fetchone()
+            properties['area_current'] = {}
 
-            if row:
-                properties['area_current'] = dict(zip(columns, row))
-            else:
-                properties['area_current'] = {}
+        return properties
+    
+    def makeOrganizationGeoSources(self, 
+                                   properties,
+                                   tolerance=0.001):
+        sites = '''
+            SELECT
+              id,
+              array_to_string(array_agg(name), ', ') AS name,
+              array_to_string(array_agg(admin_level_1_osm_name), ', ') AS admin_level_1_osm_name,
+              array_to_string(array_agg(osm_name), ', ') AS osm_name,
+              array_to_string(array_agg(date_first_cited_value), ', ') AS date_first_cited,
+              array_to_string(array_agg(date_last_cited_value), ', ') AS date_last_cited,
+              json_agg(organization_id_source)->0 AS sources,
+              ST_AsGeoJSON(
+                ST_Collect(
+                  ST_GeomFromGeoJSON(location::VARCHAR)
+                ))::json AS geometry
+            FROM (
+              SELECT DISTINCT ON (e.id)
+                g.id,
+                g.name,
+                g.admin_level_1 AS admin_level_1_osm_name,
+                g.osmname AS osm_name,
+                e.start_date_value AS date_first_cited_value,
+                e.end_date_value AS date_last_cited_value,
+                e.organization_id_source,
+                ST_AsGeoJSON(g.coordinates)::json AS location
+              FROM organization AS o
+              JOIN emplacement_sources AS e
+                ON o.id = e.organization_id_value
+              JOIN geosite AS g
+                ON e.site_id_value = g.id
+              WHERE o.id = %s
+                AND g.coordinates IS NOT NULL
+            ) AS subq
+            GROUP BY id
+        '''
 
+        q_params = [properties['id']]
+        
+        cursor = connection.cursor()
+        cursor.execute(sites, q_params)
+        columns = [c[0] for c in cursor.description]
+
+        properties['sites'] = [self.makeFeature(r[-1], dict(zip(columns, r))) for r in cursor]
+
+        areas = ''' 
+            SELECT
+              id,
+              array_to_string(array_agg(name), ', ') AS osm_name,
+              array_to_string(array_agg(osmid), ', ') AS osmid,
+              array_to_string(array_agg(first_cited_value), ', ') AS first_cited,
+              array_to_string(array_agg(last_cited_value), ', ') AS last_cited,
+              json_agg(organization_id_source)->0 AS sources,
+              ST_AsGeoJSON(
+                ST_Collect(
+                  ST_GeomFromGeoJSON(geometry::VARCHAR)
+                ))::json AS geometry
+            FROM (
+              SELECT DISTINCT ON (ass.id)
+                o.id,
+                a.name,
+                a.osmname AS osm_name,
+                a.osmid,
+                ass.start_date_value AS first_cited_value,
+                ass.end_date_value AS last_cited_value,
+                ass.organization_id_source,
+                ST_AsGeoJSON(ST_Simplify(a.geometry, %s))::json AS geometry
+              FROM organization AS o
+              JOIN association_sources AS ass
+                ON o.id = ass.organization_id_value
+              JOIN area AS a
+                ON ass.area_id_value = a.id
+              WHERE o.id = %s
+              AND a.geometry IS NOT NULL
+            ) AS subq
+            GROUP BY id
+        '''
+        
+        q_params = [tolerance, properties['id']]
+        
+        cursor = connection.cursor()
+        cursor.execute(areas, q_params)
+        columns = [c[0] for c in cursor.description]
+
+        properties['areas'] = [self.makeFeature(r[-1], dict(zip(columns, r))) for r in cursor]
+        
         return properties
 
     def makeOrganization(self,
@@ -701,8 +784,11 @@ class JSONAPIView(JSONResponseMixin, TemplateView, CacheMixin):
         if not simple:
             properties = self.makeOrganizationGeographies(properties,
                                                           when,
-                                                          all_geography=all_geography,
                                                           tolerance=tolerance)
+        
+        if all_geography:
+            properties = self.makeOrganizationGeoSources(properties, 
+                                                         tolerance=tolerance)
 
         if memberships and properties.get('memberships'):
 
