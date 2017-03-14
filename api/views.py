c60e0906202e33a3562a47c059761d9b22fbcd05
Eric van Zanten <eric.vanzanten@gmail.com>
2017-03-14 15:58:09 -0500

    Fudge date range a bit

--- api/views.py
+++ api/views.py
@@ -165,7 +165,8 @@ class EventDetailView(JSONAPIView):
                 AND CASE WHEN (ass.start_date IS NOT NULL AND ass.end_date IS NOT NULL) 
                       THEN (daterange((v.start_date::date - INTERVAL '30 days')::date, 
                                       (v.end_date::date + INTERVAL '30 days')::date) &&
-                            daterange(ass.start_date::date, ass.end_date::date))
+                            daterange((ass.start_date::date - INTERVAL '1 day')::date, 
+                                      (ass.end_date::date + INTERVAL '1 day')::date)
                     WHEN (ass.start_date IS NOT NULL AND ass.end_date IS NULL AND ass.open_ended IS FALSE)
                       THEN (ass.start_date::date 
                             BETWEEN 
