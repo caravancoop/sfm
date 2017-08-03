edcff0155f782ec56e21c2019dfdf1574718db6e
Jean Cochrane <jean@jeancochrane.com>
2017-08-03 14:50:17 -0500

    Only show changelog when multiple versions exist

--- sfm_pc/templatetags/render_versions.py
+++ sfm_pc/templatetags/render_versions.py
@@ -4,11 +4,10 @@ import itertools
 
 register = template.Library()
 
-@register.inclusion_tag('partials/version_list.html')
-def render_versions(object_ref):
-    complex_fields = [f for f in dir(object_ref) \
-                          if f.endswith('_set')]
-    
+def get_versions(object_ref):
+
+    complex_fields = [f for f in dir(object_ref) if f.endswith('_set')]
+
     versioned_types = []
 
     for field in complex_fields:
@@ -17,11 +16,32 @@ def render_versions(object_ref):
                 versions = Version.objects.get_for_object(related)
 
                 versioned_types.append((related.field_name, versions,))
-    
+
     versioned_types = sorted(versioned_types, key=lambda x: x[0])
+
+    return versioned_types
+
+@register.inclusion_tag('partials/version_list.html')
+def render_versions(object_ref):
+
     context = {'versions': []}
+
+    versioned_types = get_versions(object_ref)
 
     for object_type, grouping in itertools.groupby(versioned_types, key=lambda x: x[0]):
         context['versions'].append({object_type: list(grouping)})
 
     return context
+
+
+@register.simple_tag
+def has_versions(object_ref):
+
+    versioned_types = get_versions(object_ref)
+
+    # versioned_types is a list of tuples, so we want to sum over the second
+    # element of each tuple to get the count of versioned elements
+    count = sum(len(x[1]) for x in versioned_types if len(x[1]))
+
+    return count
+
