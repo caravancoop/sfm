52c8eb78c817033a25a062bc3f1dd184f29cd779
reginafcompton <reginafcompton@gmail.com>
2017-08-28 16:01:52 -0500

    Remove more-itertools

--- sfm_pc/templatetags/language_url.py
+++ sfm_pc/templatetags/language_url.py
@@ -1,7 +1,5 @@
 import json
 
-from more_itertools import unique_everseen
-
 from django import template
 from django.conf import settings
 from django.template.defaultfilters import stringfilter
@@ -10,16 +8,17 @@ register = template.Library()
 
 @register.filter
 def create_select2_data(select_lang_abbr):
-  # Returns an array of languages – with the current language as the first element.
-  # To configure the select2 dropdown in the correct order.
-  trans_obj = {
-      "en": "English",
-      "fr": "French",
-      "es": "Spanish",
-  }
-  data_array = list(trans_obj.values())
-  full_text = trans_obj[select_lang_abbr]
-  data_array.insert(0, full_text)
-  data_array = list(unique_everseen(data_array))
+    # Returns an array of languages – with the current language as the first element.
+    # To configure the select2 dropdown in the correct order.
+    trans_obj = {
+        "en": "English",
+        "fr": "French",
+        "es": "Spanish",
+    }
+    data_array = list(trans_obj.values())
+    full_text = trans_obj[select_lang_abbr]
 
-  return json.dumps(data_array)
+    if data_array[0] != full_text:
+        data_array.insert(0, data_array.pop(data_array.index(full_text)))
+
+    return json.dumps(data_array)
