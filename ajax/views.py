4c725ce50b023f0084aa2a05d2f1ce8d88edbe0d
reginafcompton <reginafcompton@gmail.com>
2017-09-05 10:47:45 -0500

    Progess on styles

--- ajax/views.py
+++ ajax/views.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 from jsonview.decorators import json_view
 
-from sfm_pc.utils import chain_of_command
+from sfm_pc.utils import chain_of_command, get_org_hierarchy_by_id
 
 @json_view
 def test(self, person_id):
@@ -11,4 +11,11 @@ def test(self, person_id):
 
 @json_view
 def chain(self, org_id):
-    return chain_of_command(org_id)
+    # Get edge list
+    node_list = get_org_hierarchy_by_id(org_id)
+    return node_list
+
+    # Get nodes
+    # edge_list = get_command_edges(org.uuid)
+    # context['edge_list'] = json.dumps(edge_list)
+    # return chain_of_command(org_id)
