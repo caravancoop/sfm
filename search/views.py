34fa5ebe4636c7953eeb462c1b09e36a1d306b5d
Jean Cochrane <jean@jeancochrane.com>
2017-10-03 09:14:43 -0500

    Properly strip search terms before splitting them

--- search/views.py
+++ search/views.py
@@ -106,7 +106,7 @@ def get_search_context(request, all_results=False):
     if user_query is None or user_query == '':
         full_query = '*'
     else:
-        full_query = ' '.join(term + '~' for term in user_query.split(' '))
+        full_query = ' '.join(term + '~' for term in user_query.strip().split(' '))
 
     # Filter on date params, if they exist; otherwise, don't bother
     if start_date or end_date:
