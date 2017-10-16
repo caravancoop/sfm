9dfc89a9d2c117b34d5a2d966409d74e7b144838
Jean Cochrane <jean@jeancochrane.com>
2017-10-16 14:26:26 -0500

    Add 'superiors' table to person detail (closes #263)

--- person/views.py
+++ person/views.py
@@ -45,10 +45,11 @@ class PersonDetail(DetailView):
         context['download_url'] = reverse('download') + params
 
         affiliations = context['person'].memberships
-        memberships = [mem.object_ref for mem in affiliations]
+        memberships = tuple(mem.object_ref for mem in affiliations)
 
         context['memberships'] = memberships
         context['subordinates'] = []
+        context['superiors'] = []
         context['command_chain'] = []
 
         for membership in memberships:
@@ -81,132 +82,25 @@ class PersonDetail(DetailView):
             child_compositions = org.child_organization.all()
 
             if child_compositions:
+                child_commanders = get_commanders(membership,
+                                                  child_compositions,
+                                                  relationship='child')
+                if child_commanders:
+                    context['subordinates'] += child_commanders
 
-                # Get start and end date for this membership, to determine
-                # overlap
-                mem_start = membership.firstciteddate.get_value()
-                no_start = False
-                if mem_start is None:
-                    # Make a bogus date that everything will be greater than
-                    mem_start = date(1000, 1, 1)
-                    no_start = True
-                else:
-                    mem_start = repr(mem_start.value)
+            parent_compositions = org.parent_organization.all()
 
-                mem_end = membership.lastciteddate.get_value()
-                no_end = False
-                if mem_end is None:
-                    mem_end = date.today()
-                    no_end = True
-                else:
-                    mem_end = repr(mem_end.value)
+            if parent_compositions:
+                parent_commanders = get_commanders(membership,
+                                                   parent_compositions,
+                                                   relationship='parent')
 
-                for composition in child_compositions:
-                    # Start and end date attributes for filtering:
-                    # We want only the personnel who were commanders of child
-                    # organizations during this membership
-                    # (also allowing for null dates)
-                    child = composition.object_ref.child.get_value()
-                    child_id = child.value.uuid
+                if parent_commanders:
+                    context['superiors'] += parent_commanders
 
-                    child_commanders_query = '''
-                        SELECT * FROM membershipperson
-                        WHERE organization_id='{child_id}'
-                        AND (first_cited <= '{mem_end}' or first_cited is Null)
-                        AND (last_cited >= '{mem_start}' or last_cited is Null)
-                    '''.format(child_id=child_id,
-                               mem_end=mem_end,
-                               mem_start=mem_start)
+        context['subordinates'] = sort_commanders(context['subordinates'])
+        context['superiors'] = sort_commanders(context['superiors'])
 
-                    cursor = connection.cursor()
-                    cursor.execute(child_commanders_query)
-
-                    columns = [c[0] for c in cursor.description]
-                    results_tuple = namedtuple('Commander', columns)
-
-                    commanders = [results_tuple(*r) for r in cursor]
-
-                    for commander_tuple in commanders:
-                        # We need to calculate time overlap, so use a dict to
-                        # stash information about this commander
-                        commander = MembershipPersonMember.objects.get(object_ref__id=commander_tuple.id)
-                        info = {}
-                        info['commander'] = commander.object_ref
-                        info['organization'] = child
-
-                        # Get start of the overlap between these two people,
-                        # being sensitive to nulls
-
-                        # First, try to get the commander of the child unit's
-                        # start/end dates
-                        c_start = commander.object_ref.firstciteddate.get_value()
-                        c_end = commander.object_ref.lastciteddate.get_value()
-
-                        if c_start and not no_end:
-                            overlap_start = c_start
-                        else:
-                            # Once we have "ongoing" attributes, we'll be able to
-                            # determine ongoing overlap; for now, mark it as "unknown"
-                            overlap_start = _('Unknown')
-
-                        if c_end and not no_start:
-                            overlap_end = c_end
-                        else:
-                            # Ditto about "ongoing" attributes above
-                            overlap_end = _('Unknown')
-
-                        if overlap_start != _('Unknown') and overlap_end != _('Unknown'):
-
-                            # Convert to date objects to calculate delta
-                            start_year = overlap_start.value.year
-                            start_month = overlap_start.value.month
-                            start_day = overlap_start.value.day
-
-                            end_year = overlap_end.value.year
-                            end_month = overlap_end.value.month
-                            end_day = overlap_end.value.day
-
-                            # Account for fuzzy dates
-                            all_dates = [start_year, start_month, start_day,
-                                         end_year, end_month, end_day]
-
-                            fuzzy_date =  any(dt == 0 for dt in all_dates)
-
-                            if fuzzy_date:
-                                # Start the overlap string with a "roughly" symbol
-                                overlap_duration = '~'
-
-                                # Find spots where the dates are fuzzy
-                                if start_month == 0:
-                                    start_month = 1
-                                if start_day == 0:
-                                    start_day = 1
-                                if end_month == 0:
-                                    end_month = 1
-                                if end_day == 0:
-                                    end_day = 1
-                            else:
-                                overlap_duration = ''
-
-                            start = date(start_year,
-                                         start_month,
-                                         start_day)
-
-                            end = date(end_year,
-                                       end_month,
-                                       end_day)
-
-                            overlap_duration += (str((end - start).days) + ' ' + _('days'))
-                        else:
-                            overlap_duration = _('Unknown')
-
-                        info['overlap_start'] = overlap_start
-                        info['overlap_end'] = overlap_end
-                        info['overlap_duration'] = overlap_duration
-
-                        context['subordinates'].append(info)
-
-        context['subordinates'] = sorted(context['subordinates'], key=lambda m: (m['overlap_end'].value if m['overlap_end'] != _('Unknown') else date(1, 1, 1)), reverse=True)
         context['command_chain'].reverse()
         context['events'] = []
         events = context['person'].violationperpetrator_set.all()
@@ -214,6 +108,151 @@ class PersonDetail(DetailView):
             context['events'].append(event.object_ref)
 
         return context
+
+
+def get_commanders(membership, compositions, relationship='child'):
+
+    assert relationship in ('parent', 'child')
+
+    comms = []
+
+    # Get start and end date for this membership, to determine
+    # overlap
+    mem_start = membership.firstciteddate.get_value()
+    no_start = False
+    if mem_start is None:
+        # Make a bogus date that everything will be greater than
+        mem_start = date(1000, 1, 1)
+        no_start = True
+    else:
+        mem_start = repr(mem_start.value)
+
+    mem_end = membership.lastciteddate.get_value()
+    no_end = False
+    if mem_end is None:
+        mem_end = date.today()
+        no_end = True
+    else:
+        mem_end = repr(mem_end.value)
+
+    for composition in compositions:
+        # Start and end date attributes for filtering:
+        # We want only the personnel who were commanders of child
+        # organizations during this membership
+        # (also allowing for null dates)
+        child = getattr(composition.object_ref, relationship)
+        child = child.get_value()
+        child_id = child.value.uuid
+
+        child_commanders_query = '''
+            SELECT * FROM membershipperson
+            WHERE organization_id='{child_id}'
+            AND (first_cited <= '{mem_end}' or first_cited is Null)
+            AND (last_cited >= '{mem_start}' or last_cited is Null)
+        '''.format(child_id=child_id,
+                    mem_end=mem_end,
+                    mem_start=mem_start)
+
+        cursor = connection.cursor()
+        cursor.execute(child_commanders_query)
+
+        columns = [c[0] for c in cursor.description]
+        results_tuple = namedtuple('Commander', columns)
+
+        commanders = [results_tuple(*r) for r in cursor]
+
+        for commander_tuple in commanders:
+            # We need to calculate time overlap, so use a dict to
+            # stash information about this commander
+            commander = MembershipPersonMember.objects.get(object_ref__id=commander_tuple.id)
+            info = {}
+            info['commander'] = commander.object_ref
+            info['organization'] = child
+
+            # Get start of the overlap between these two people,
+            # being sensitive to nulls
+
+            # First, try to get the commander of the child unit's
+            # start/end dates
+            c_start = commander.object_ref.firstciteddate.get_value()
+            c_end = commander.object_ref.lastciteddate.get_value()
+
+            if c_start and not no_end:
+                overlap_start = c_start
+            else:
+                # Once we have "ongoing" attributes, we'll be able to
+                # determine ongoing overlap; for now, mark it as "unknown"
+                overlap_start = _('Unknown')
+
+            if c_end and not no_start:
+                overlap_end = c_end
+            else:
+                # Ditto about "ongoing" attributes above
+                overlap_end = _('Unknown')
+
+            if overlap_start != _('Unknown') and overlap_end != _('Unknown'):
+
+                # Convert to date objects to calculate delta
+                start_year = overlap_start.value.year
+                start_month = overlap_start.value.month
+                start_day = overlap_start.value.day
+
+                end_year = overlap_end.value.year
+                end_month = overlap_end.value.month
+                end_day = overlap_end.value.day
+
+                # Account for fuzzy dates
+                all_dates = [start_year, start_month, start_day,
+                                end_year, end_month, end_day]
+
+                fuzzy_date =  any(dt == 0 for dt in all_dates)
+
+                if fuzzy_date:
+                    # Start the overlap string with a "roughly" symbol
+                    overlap_duration = '~'
+
+                    # Find spots where the dates are fuzzy
+                    if start_month == 0:
+                        start_month = 1
+                    if start_day == 0:
+                        start_day = 1
+                    if end_month == 0:
+                        end_month = 1
+                    if end_day == 0:
+                        end_day = 1
+                else:
+                    overlap_duration = ''
+
+                start = date(start_year,
+                                start_month,
+                                start_day)
+
+                end = date(end_year,
+                            end_month,
+                            end_day)
+
+                overlap_duration += (str((end - start).days) + ' ' + _('days'))
+            else:
+                overlap_duration = _('Unknown')
+
+            info['overlap_start'] = overlap_start
+            info['overlap_end'] = overlap_end
+            info['overlap_duration'] = overlap_duration
+
+            comms.append(info)
+
+    return comms
+
+
+def sort_commanders(commanders):
+
+    if commanders:
+        return sorted(commanders,
+                    key=lambda m: (m['overlap_end'].value if m['overlap_end'] != _('Unknown')
+                                    else date(1, 1, 1)),
+                    reverse=True)
+    else:
+        return commanders
 
 
 class PersonList(PaginatedList):
