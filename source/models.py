86b922a45da260614e38fdcb19922a5e32e5e762
Eric van Zanten <eric@e-vz.com>
2018-07-06 08:50:22 -0500

    Alter relationships between attributes and sources

--- source/models.py
+++ source/models.py
@@ -64,6 +64,7 @@ class Source(models.Model):
 class AccessPoint(models.Model):
     # TODO: Change PK to UUID and alter complex field relationships to point to
     # this table instead of source table
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4)
     page_number = models.CharField(max_length=255, null=True, blank=True)
     accessed_on = models.DateField(null=True, blank=True)
     archive_url = models.URLField(max_length=1000, null=True, blank=True)
