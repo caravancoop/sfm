5a5e48183123e15ef09cfda4c885a7553b632f32
Eric van Zanten <eric@e-vz.com>
2018-07-31 15:44:27 -0500

    remove caching for person view

--- person/urls.py
+++ person/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
         name="create-person"),
     url(r'edit/(?P<slug>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/$', PersonEditView.as_view(), name='edit-person'),
     url(r'^view/(?P<slug>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/$',
-        cache_page(60 * 60 * 24)(PersonDetail.as_view()),
+        PersonDetail.as_view(),
         name="view-person"),
 ]
