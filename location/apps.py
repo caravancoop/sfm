7a53224d925ca588dcc686067ddd8a027a78d00f
Eric van Zanten <eric@e-vz.com>
2018-11-13 15:27:02 -0600

    A bunch of location related fixes

--- location/apps.py
+++ location/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class LocationConfig(AppConfig):
     name = 'location'
+
+    def ready(self):
+        from location import signals
--- location/models.py
+++ location/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.gis.db.models.fields import GeometryField
 from django.contrib.postgres.fields import JSONField
+from django.core.urlresolvers import reverse
 
 class Location(models.Model):
     id = models.BigIntegerField(primary_key=True)
@@ -8,6 +9,15 @@ class Location(models.Model):
     division_id = models.TextField(blank=True, null=True)
     feature_type = models.TextField(blank=True, null=True)
     tags = JSONField(blank=True, null=True)
+    adminlevel1 = models.ForeignKey('self',
+                                    related_name='area_locations',
+                                    null=True,
+                                    blank=True)
+    adminlevel2 = models.ForeignKey('self',
+                                    related_name='place_locations',
+                                    null=True,
+                                    blank=True)
+    adminlevel = models.CharField(max_length=5, null=True, blank=True)
     geometry = GeometryField(blank=True, null=True)
 
     def __str__(self):
