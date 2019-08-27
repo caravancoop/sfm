30f20397c185595af299a2e708602748dd75e971
Jean Cochrane (Lead developer, DataMade) <jean.cochrane@datamade.us>
2019-08-27 16:19:54 -0500

    Allow negative Location IDs

--- location/urls.py
+++ location/urls.py
@@ -4,8 +4,8 @@ from location.views import LocationView, LocationList, LocationCreate, \
     LocationDelete, LocationAutoComplete
 
 urlpatterns = [
-    url(r'^delete/(?P<pk>\d+)$', LocationDelete.as_view(), name="delete-location"),
-    url(r'^view/(?P<pk>\d+)$', LocationView.as_view(), name="view-location"),
+    url(r'^delete/(?P<pk>-?\d+)$', LocationDelete.as_view(), name="delete-location"),
+    url(r'^view/(?P<pk>-?\d+)$', LocationView.as_view(), name="view-location"),
     url(r'^create/$', LocationCreate.as_view(), name='create-location'),
     url(r'^autocomplete/$', LocationAutoComplete.as_view(), name='location-autocomplete'),
     url(r'^$', LocationList.as_view(), name='list-location'),
