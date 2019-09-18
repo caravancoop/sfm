7802ee47a24bd95867661bd5e4b5d0d1b5ad9e02
Jean Cochrane (Lead developer, DataMade) <jean.cochrane@datamade.us>
2019-09-18 10:34:48 -0500

    Add test_relationships to importer test

--- tests/test_importer.py
+++ tests/test_importer.py
@@ -66,3 +66,50 @@ def test_sources(data_import):
                         assert not sources & seen_sources
                         seen_sources = seen_sources | sources
                         seen_attrs.append(attr)
+
+
+@pytest.mark.django_db
+def test_relationships(data_import):
+    """Make sure the correct relationships between entities get imported."""
+    # Test org compositions
+    org_nodes = ('Alpha', 'Beta', 'Gamma', 'Delta')
+    org_edges = (None, 'Alpha', 'Beta', 'Gamma')
+    for node, edge in zip(org_nodes, org_edges):
+        org = Organization.objects.get(
+            # Orgs in the source data have trailing spaces in their names
+            organizationname__value='Importer Test Organization {} Name '.format(node)
+        )
+        if edge:
+            assert org.parent_organization.count() == 1
+            expected_parent_name = 'Importer Test Organization {} Name '.format(edge)
+            parent_org = org.parent_organization.first().object_ref.parent.get_value().value
+            assert parent_org.name.get_value().value == expected_parent_name
+        else:
+            assert org.parent_organization.count() == 0
+            assert org.child_organization.count() == 1
+
+    # Test org memberships
+    is_member_org = Organization.objects.get(
+        organizationname__value='Is-member Organization name'
+    )
+    has_member_org = Organization.objects.get(
+        organizationname__value='Has-member Organization name'
+    )
+    assert is_member_org.membershiporganizationmember_set.count() == 1
+    assert has_member_org.membershiporganizationorganization_set.count() == 1
+
+    membership = is_member_org.membershiporganizationmember_set.first().object_ref
+    member_org = membership.organization.get_value().value
+    assert member_org == has_member_org
+
+    # Test chain of command
+    person_nodes = org_nodes
+    person_edges = person_nodes
+    for node, edge in zip(person_nodes, person_edges):
+        person = Person.objects.get(
+            personname__value='Importer Test Person {} Name'.format(node)
+        )
+        assert person.memberships.count() == 1
+        member_org = person.memberships.first().object_ref.organization.get_value().value
+        expected_org_name = 'Importer Test Organization {} Name '.format(edge)
+        assert member_org.name.get_value().value == expected_org_name
