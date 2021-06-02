7f5bb2497c7ed128f3cbe4607fe2e414a52f94c4
hancush <hannah.cushman@gmail.com>
2021-06-02 14:43:22 -0500

    Track source table link clicks

--- static/js/piwik_tracker.js
+++ static/js/piwik_tracker.js
@@ -3,3 +3,4 @@
 // The good stuff is in piwik_tracker_production.js. We decrypt that one and
 // move it over here when we deploy to production, to avoid sending dev data
 // to Piwik.
+var _paq = [];
