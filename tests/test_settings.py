9102e8064b408efa934d59af913f4c49f0ce840c
hancush <hannah.cushman@gmail.com>
2021-07-08 13:23:37 -0500

    Strike stray modeltranslation reference

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -29,7 +29,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = (
     'django.contrib.sites',
-    'modeltranslation',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
