ae657010d04697bd8a2ff754fb97022b4037a2f2
Sam McAlilly <smcalilly@gmail.com>
2022-11-02 10:21:43 -0500

    better dict reader with stdout

--- data/processors/blank_columns.py
+++ data/processors/blank_columns.py
@@ -8,17 +8,15 @@ parser.add_argument('--entity', type=str, required=True)
 args = parser.parse_args()
 
 # init the incoming data as a dict reader
-lines = iter(line.decode('utf-8').strip() for line in sys.stdin.buffer.readlines())
-header = next(lines).split(',')
-reader = csv.DictReader(lines, fieldnames=header)
+reader = csv.DictReader(sys.stdin)
 
 # write data to stdout
-stdout_csv = csv.writer(sys.stdout)
-stdout_csv.writerow(header)
+stdout_csv = csv.DictWriter(sys.stdout, fieldnames=reader.fieldnames)
+stdout_csv.writeheader()
 
 for row in reader:
     row.update({
         f'{args.entity}:comments:admin': '',
         f'{args.entity}:owner:admin': ''
     })
-    stdout_csv.writerow(row.values())
+    stdout_csv.writerow(row)
