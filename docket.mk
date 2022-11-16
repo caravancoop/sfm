e5b6f13bbbd5923dbd43355e76e5757cc95a5d1a
Sam McAlilly <smcalilly@gmail.com>
2022-11-16 16:19:27 -0600

    better name

--- docket.mk
+++ docket.mk
@@ -1,12 +1,12 @@
 # Path variable for the source data
-SOURCE_DATA_TARGET_PATH=sfm_pc/management/commands/country_data/countries
+SOURCE_DATA_PATH=sfm_pc/management/commands/country_data/countries
 
 # Variables for the archive data
-DATA_ARCHIVE_TARGET_PATH=data/wwic_download/countries
+DATA_ARCHIVE_PATH=data/wwic_download/countries
 COUNTRY_NAMES=$(shell perl -pe "s/,/ /g" import_docket.csv | cut -d' ' -f5)
 ENTITIES=units.csv persons.csv incidents.csv locations.csv locations.geojson sources.csv
 
-.PHONY: $(SOURCE_DATA_TARGET_PATH) data/wwic_download/countries data_archive wwic_download.zip directories data/wwic_download/metadata/sfm_research_handbook.pdf
+.PHONY: $(SOURCE_DATA_PATH) data/wwic_download/countries data_archive wwic_download.zip directories data/wwic_download/metadata/sfm_research_handbook.pdf
 
 
 # Create the data archive and upload it to S3
@@ -17,41 +17,41 @@ data_archive : wwic_download.zip
 wwic_download.zip : filtered_data data/wwic_download/metadata/sfm_research_handbook.pdf
 	cd data/wwic_download && zip -r ../../$@ .
 
-filtered_data: directories $(SOURCE_DATA_TARGET_PATH) $(foreach country,$(COUNTRY_NAMES),$(patsubst %,$(country)_%,$(ENTITIES)))
+filtered_data: directories $(SOURCE_DATA_PATH) $(foreach country,$(COUNTRY_NAMES),$(patsubst %,$(country)_%,$(ENTITIES)))
 	echo "filtered csvs for entities"
 
 directories : 
-	mkdir -p $(foreach country,$(COUNTRY_NAMES),$(DATA_ARCHIVE_TARGET_PATH)/$(country))
+	mkdir -p $(foreach country,$(COUNTRY_NAMES),$(DATA_ARCHIVE_PATH)/$(country))
 
 define filter_entity_data
 	$(shell csvgrep --columns $(1):status:admin --match 3 $< | \
-					python data/processors/blank_columns.py --entity $(1) > $(DATA_ARCHIVE_TARGET_PATH)/$*/$@)
+					python data/processors/blank_columns.py --entity $(1) > $(DATA_ARCHIVE_PATH)/$*/$@)
 endef
 
-%_units.csv : $(SOURCE_DATA_TARGET_PATH)/%/units.csv
+%_units.csv : $(SOURCE_DATA_PATH)/%/units.csv
 	$(call filter_entity_data,unit)
 
-%_persons.csv : $(SOURCE_DATA_TARGET_PATH)/%/persons.csv
+%_persons.csv : $(SOURCE_DATA_PATH)/%/persons.csv
 	$(call filter_entity_data,person)
 
-%_incidents.csv : $(SOURCE_DATA_TARGET_PATH)/%/incidents.csv
+%_incidents.csv : $(SOURCE_DATA_PATH)/%/incidents.csv
 	$(call filter_entity_data,incident)
 
-%_sources.csv : $(SOURCE_DATA_TARGET_PATH)/%/sources.csv
-	cp $< $(DATA_ARCHIVE_TARGET_PATH)/$*/$@
+%_sources.csv : $(SOURCE_DATA_PATH)/%/sources.csv
+	cp $< $(DATA_ARCHIVE_PATH)/$*/$@
 
-%_locations.csv : $(SOURCE_DATA_TARGET_PATH)/%/locations.csv
-	cp $< $(DATA_ARCHIVE_TARGET_PATH)/$*/$@
+%_locations.csv : $(SOURCE_DATA_PATH)/%/locations.csv
+	cp $< $(DATA_ARCHIVE_PATH)/$*/$@
 
-%_locations.geojson : $(SOURCE_DATA_TARGET_PATH)/%/locations.geojson
-	cp $< $(DATA_ARCHIVE_TARGET_PATH)/$*/$@
+%_locations.geojson : $(SOURCE_DATA_PATH)/%/locations.geojson
+	cp $< $(DATA_ARCHIVE_PATH)/$*/$@
 
 data/wwic_download/metadata/sfm_research_handbook.pdf : 
 	curl -o $@ https://help.securityforcemonitor.org/_/downloads/en/latest/pdf/
 
 
 # Download the source data and load it into the database
-%_import : %.csv $(SOURCE_DATA_TARGET_PATH)
+%_import : %.csv $(SOURCE_DATA_PATH)
 	perl -pe "s/,/ /g" $< | \
 	xargs -L1 bash -c ' \
 		echo "Loading data for country code $$3" && (\
@@ -62,7 +62,7 @@ data/wwic_download/metadata/sfm_research_handbook.pdf :
 			exit 255 \
 		)'
 
-$(SOURCE_DATA_TARGET_PATH) : import_docket.csv
+$(SOURCE_DATA_PATH) : import_docket.csv
 	perl -pe "s/,/ /g" $< | \
 	xargs -L1 bash -c ' \
 		echo "Downloading data for country $$4" && (\
